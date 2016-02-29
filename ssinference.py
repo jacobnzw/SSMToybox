410eb4605e4db4c3aa2e2e11edd309803211f56d
Jakub <jacobnzw@gmail.com>
2016-02-29 13:05:29 +0200

    Variables now initialized to None in __init__(), renamed long variables, cleaned forward_pass() and backward_pass().

--- ssinference.py
+++ ssinference.py
@@ -7,12 +7,19 @@ class StateSpaceInference(object):
 
     def __init__(self, trans, sys):
         self.trans = trans  # transformation decides which filter will be used
-        self.sys = sys  # system whose state is to be estimated
+        self.sys = sys  # dynamical system whose state is to be estimated
+        # set initial condition mean and covariance, and noise covariances
         self.x_mean_filt, self.x_cov_filt, self.q_cov, self.r_cov = sys.get_pars(
                 'x0_mean', 'x0_cov', 'q_cov', 'r_cov'
         )
-
-        self.flags = {'filtered': False}
+        self.flags = {'filtered': False, 'smoothed': False}
+        self.x_mean_pred, self.x_cov_pred, = None, None
+        self.x_mean_smooth, self.x_cov_smooth = None, None
+        self.xx_cov, self.xz_cov = None, None
+        self.pr_mean, self.pr_cov, self.pr_xx_cov = None, None, None
+        self.fi_mean, self.fi_cov = None, None
+        self.sm_mean, self.sm_cov = None, None
+        self.D, self.N = None, None
 
     def get_flag(self, key):
         return self.flags[key]
@@ -21,41 +28,40 @@ class StateSpaceInference(object):
         self.flags[key] = value
 
     def forward_pass(self, data):
-        D, N = data.shape
-        self.filt_state_mean = np.empty((D, N))
-        self.filt_state_cov = np.empty((D, D, N))
-        self.pred_state_mean = self.filt_state_mean.copy()
-        self.pred_state_cov = self.filt_state_cov.copy()
-        self.pred_state_xx_cov = self.filt_state_cov.copy()
-        for k in xrange(data.shape[-1]):  # iterate over columns of data
+        self.D, self.N = data.shape
+        self.fi_mean = np.empty((self.D, self.N))
+        self.fi_cov = np.empty((self.D, self.D, self.N))
+        self.pr_mean = self.fi_mean.copy()
+        self.pr_cov = self.fi_cov.copy()
+        self.pr_xx_cov = self.fi_cov.copy()
+        for k in xrange(self.N):  # iterate over columns of data
             self._time_update(k)
-            (self.pred_state_mean[..., k],
-             self.pred_state_cov[..., k],
-             self.pred_state_xx_cov[..., k]) = self.x_mean_pred, self.x_cov_pred, self.xx_cov
+            self.pr_mean[..., k] = self.x_mean_pred
+            self.pr_cov[..., k] = self.x_cov_pred
+            self.pr_xx_cov[..., k] = self.xx_cov
             self._measurement_update(data[:, k])
-            self.filt_state_mean[..., k], self.filt_state_cov[..., k] = self.x_mean_filt, self.x_cov_filt
+            self.fi_mean[..., k], self.fi_cov[..., k] = self.x_mean_filt, self.x_cov_filt
         # set flag that filtered state sequence is available
         self.set_flag('filtered', True)
+        # smoothing estimate at the last time step == the filtering estimate at the last time step
         self.x_mean_smooth, self.x_cov_smooth = self.x_mean_filt, self.x_cov_filt
-        return self.filt_state_mean, self.filt_state_cov
+        return self.fi_mean, self.fi_cov
 
     def backward_pass(self):
-        assert self.get_flag('filtered')  # filtered state is required for now, condition in the future?
-        D, N = self.filt_state_mean.shape
-        smoothed_state_mean = np.empty((D, N))
-        smoothed_state_cov = np.empty((D, D, N))
-        smoothed_state_mean[..., -1] = self.x_mean_filt
-        smoothed_state_cov[..., -1] = self.x_cov_filt
-        for k in xrange(N-2, 0, -1):
-            self.x_mean_pred = self.pred_state_mean[..., k+1]
-            self.x_cov_pred = self.pred_state_cov[..., k+1]
-            self.xx_cov = self.pred_state_xx_cov[..., k+1]
-            self.x_mean_filt = self.filt_state_mean[..., k]
-            self.x_cov_filt = self.filt_state_cov[..., k]
+        assert self.get_flag('filtered')  # require filtered state
+        self.sm_mean = self.fi_mean.copy()
+        self.sm_cov = self.fi_cov.copy()
+        for k in xrange(self.N-2, 0, -1):
+            self.x_mean_pred = self.pr_mean[..., k+1]
+            self.x_cov_pred = self.pr_cov[..., k+1]
+            self.xx_cov = self.pr_xx_cov[..., k+1]
+            self.x_mean_filt = self.fi_mean[..., k]
+            self.x_cov_filt = self.fi_cov[..., k]
             self._smoothing_update()
-            smoothed_state_mean[..., k] = self.x_mean_smooth
-            smoothed_state_cov[..., k] = self.x_cov_smooth
-        return smoothed_state_mean, smoothed_state_cov
+            self.sm_mean[..., k] = self.x_mean_smooth
+            self.sm_cov[..., k] = self.x_cov_smooth
+        self.set_flag('smoothed', True)
+        return self.sm_mean, self.sm_cov
 
     def _time_update(self, time):
         # calculate predictive moments of the system state by applying moment transformation
@@ -95,14 +101,16 @@ def main():
     system = UNGM(q_cov=10, r_cov=.1)
     time_steps = 50
     X, Z = system.simulate(time_steps, 1)  # get some data from the system
+    # plot trajectory with measurements
+    import matplotlib.pyplot as plt
+    # plt.figure()
+    # plt.plot(X[0, ...], color='b', alpha=0.15)
+    # plt.plot(Z[0, ...], color='k', alpha=0.25, ls='None', marker='.')
+
     infer = UKFS(system)
     mean_f, cov_f = infer.forward_pass(Z[..., 0])
     mean_s, cov_s = infer.backward_pass()
 
-    # plot trajectory with measurements
-    import matplotlib.pyplot as plt
-    # plt.plot(X[0, ...], color='b', alpha=0.15)
-    # plt.plot(Z[0, ...], color='k', alpha=0.25, ls='None', marker='.')
     plt.figure()
     plt.plot(X[0, :, 0], color='r', ls='--')
     plt.plot(Z[0, :, 0], color='k', ls='None', marker='o')
