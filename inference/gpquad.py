Important fixes: _time_update() adds the noise covariance BEFORE the second transform is called (this was a gross error previously); changed indexing in the forward_pass(), simulate() now initializes variables for state trajectory and measurement using np.zeros() rather than np.empty() GPQuad.apply() cross-covariance computation fixed (pissed me off long enough!!!): the error was in dx.dot(Wcc).dot(dx) when it should be dx.dot(Wcc).dot(fx).

inference/gpquad.py
@@ -15,13 +15,13 @@ class GPQuadKalman(StateSpaceInference):
         assert isinstance(sys, StateSpaceModel)
         nq = sys.xD if sys.q_additive else sys.xD + sys.qD
         nr = sys.xD if sys.r_additive else sys.xD + sys.rD
-        unit_sp_f = Unscented.unit_sigma_points(nq, np.sqrt(nq + 1))
-        unit_sp_h = Unscented.unit_sigma_points(nr, np.sqrt(nr + 1))
+        unit_sp_f = Unscented.unit_sigma_points(nq, np.sqrt(nq + 0))
+        unit_sp_h = Unscented.unit_sigma_points(nr, np.sqrt(nr + 0))
         hypers_f = {'sig_var': 1.0, 'lengthscale': 3.0 * np.ones((nq, 1)), 'noise_var': 1e-8}
         hypers_h = {'sig_var': 1.0, 'lengthscale': 3.0 * np.ones((nr, 1)), 'noise_var': 1e-8}
-        tf = GPQuad(unit_sp_f, hypers_f)
-        th = GPQuad(unit_sp_h, hypers_h)
-        super(GPQuadKalman, self).__init__(tf, th, sys)
+        self.tf = GPQuad(unit_sp_f, hypers_f)
+        self.th = GPQuad(unit_sp_h, hypers_h)
+        super(GPQuadKalman, self).__init__(self.tf, self.th, sys)
 
 
 def main():
@@ -43,17 +43,18 @@ def main():
 
     import matplotlib.pyplot as plt
     plt.figure()
+    time = range(1, time_steps)
     plt.plot(x[0, :, 0], color='r', ls='--', label='true state')
     plt.plot(z[0, :, 0], color='k', ls='None', marker='o')
     plt.plot(mean_f[0, ...], color='b', label='filtered estimate')
-    plt.fill_between(range(0, time_steps),
-                     mean_f[0, ...] - 2 * np.sqrt(cov_f[0, 0, :]),
-                     mean_f[0, ...] + 2 * np.sqrt(cov_f[0, 0, :]),
+    plt.fill_between(time,
+                     mean_f[0, 1:] - 2 * np.sqrt(cov_f[0, 0, 1:]),
+                     mean_f[0, 1:] + 2 * np.sqrt(cov_f[0, 0, 1:]),
                      color='b', alpha=0.15)
     plt.plot(mean_s[0, ...], color='g', label='smoothed estimate')
-    plt.fill_between(range(0, time_steps),
-                     mean_s[0, ...] - 2 * np.sqrt(cov_s[0, 0, :]),
-                     mean_s[0, ...] + 2 * np.sqrt(cov_s[0, 0, :]),
+    plt.fill_between(time,
+                     mean_s[0, 1:] - 2 * np.sqrt(cov_s[0, 0, 1:]),
+                     mean_s[0, 1:] + 2 * np.sqrt(cov_s[0, 0, 1:]),
                      color='g', alpha=0.25)
     plt.legend()
 

inference/ssinfer.py
@@ -41,8 +41,8 @@ class StateSpaceInference(object):
         self.pr_mean = self.fi_mean.copy()
         self.pr_cov = self.fi_cov.copy()
         self.pr_xx_cov = self.fi_cov.copy()
-        for k in xrange(self.N):  # iterate over columns of data
-            self._time_update(k)
+        for k in xrange(1, self.N):  # iterate over columns of data
+            self._time_update(k - 1)
             self.pr_mean[..., k] = self.x_mean_pred
             self.pr_cov[..., k] = self.x_cov_pred
             self.pr_xx_cov[..., k] = self.xx_cov
@@ -74,25 +74,27 @@ class StateSpaceInference(object):
         # in non-additive case, augment mean and covariance
         mean = self.x_mean_filt if self.sys.q_additive else np.vstack((self.x_mean_filt, self.q_mean))
         cov = self.x_cov_filt if self.sys.q_additive else block_diag(self.x_cov_filt, self.q_cov)
+        # apply moment transform to compute predicted state mean, covariance
         self.x_mean_pred, self.x_cov_pred, self.xx_cov = self.transf_dyn.apply(
                 self.sys.dyn_eval, mean, cov, self.sys.par_fcn(time)
         )
+        if self.sys.q_additive: self.x_cov_pred += self.q_cov
         # in non-additive case, augment mean and covariance
         mean = self.x_mean_pred if self.sys.r_additive else np.vstack((self.x_mean_pred, self.r_mean))
         cov = self.x_cov_pred if self.sys.r_additive else block_diag(self.x_cov_pred, self.r_cov)
-        self.mean_z_pred, self.z_cov_pred, self.xz_cov = self.transf_meas.apply(
+        # apply moment transform to compute measurement mean, covariance
+        self.z_mean_pred, self.z_cov_pred, self.xz_cov = self.transf_meas.apply(
                 self.sys.meas_eval, mean, cov, self.sys.par_fcn(time)
         )
+        # in additive case, noise covariances need to be added
+        if self.sys.r_additive: self.z_cov_pred += self.r_cov
         # in non-additive case, cross-covariances must be trimmed (has no effect in additive case)
         self.xz_cov = self.xz_cov[:, :self.sys.xD]
         self.xx_cov = self.xx_cov[:, :self.sys.xD]
-        # in additive case, noise covariances need to be added
-        if self.sys.q_additive: self.x_cov_pred += self.q_cov
-        if self.sys.r_additive: self.z_cov_pred += self.r_cov
 
     def _measurement_update(self, y):
         gain = cho_solve(cho_factor(self.z_cov_pred), self.xz_cov)
-        self.x_mean_filt = self.x_mean_pred + gain.dot(y - self.mean_z_pred)
+        self.x_mean_filt = self.x_mean_pred + gain.dot(y - self.z_mean_pred)
         self.x_cov_filt = self.x_cov_pred - gain.dot(self.z_cov_pred).dot(gain.T)
 
     def _smoothing_update(self):

models/ssmodel.py
@@ -56,8 +56,8 @@ class StateSpaceModel(object):
         x0_mean, x0_cov, q_mean, q_cov, r_mean, r_cov = self.get_pars(
                 'x0_mean', 'x0_cov', 'q_mean', 'q_cov', 'r_mean', 'r_cov'
         )
-        x = np.empty((self.xD, steps, mc_sims))
-        z = np.empty((self.zD, steps, mc_sims))
+        x = np.zeros((self.xD, steps, mc_sims))
+        z = np.zeros((self.zD, steps, mc_sims))
         q = np.random.multivariate_normal(q_mean, q_cov, size=(mc_sims, steps)).T
         r = np.random.multivariate_normal(r_mean, r_cov, size=(mc_sims, steps)).T
         x0 = np.random.multivariate_normal(x0_mean, x0_cov, size=mc_sims).T  # (D, mc_sims)

transforms/bayesquad.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import newaxis as na
-from numpy.linalg import det, inv
+from numpy.linalg import det, inv, cholesky
 from scipy.linalg import cho_factor, cho_solve
 
 from transform import MomentTransform
@@ -19,7 +19,7 @@ class GPQuad(MomentTransform):
 
     def apply(self, f, mean, cov, *args):
         # form sigma-points from unit sigma-points
-        x = mean + np.linalg.cholesky(cov).dot(self.unit_sp)
+        x = mean + cholesky(cov).dot(self.unit_sp)
         # push sigma-points through non-linearity
         fx = np.apply_along_axis(f, 0, x, *args)
         # output mean
@@ -29,7 +29,7 @@ class GPQuad(MomentTransform):
         cov_f += self.model_var
         # input-output covariance
         dx = x - mean
-        cov_fx = self.D.dot(dx).dot(self.Wcc).dot(dx.T)
+        cov_fx = self.D.dot(dx).dot(self.Wcc).dot(fx.T)
         return mean_f, cov_f, cov_fx
 
     def weights_rbf(self):