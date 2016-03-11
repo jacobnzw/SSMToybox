dyn_eval and meas_eval now return Jacobians, which are at_least_2d() arrays, EKF still does not work due to posdef errors in the UNGMnonadd test. I suspect Jacobians are wrong. For that reason, I simplified the UNGMnonadd functions.

models/ssmodel.py
@@ -42,14 +42,14 @@ class StateSpaceModel(object):
         if self.q_additive:
             assert len(xq) == self.xD
             if dx:
-                out = self.dyn_fcn_dx(xq, 0, pars).flatten()
+                out = np.atleast_2d(self.dyn_fcn_dx(xq, 0, pars).flatten())
             else:
                 out = self.dyn_fcn(xq, 0, pars)
         else:
             assert len(xq) == self.xD + self.qD
             x, q = xq[:self.xD], xq[-self.qD:]
             if dx:
-                out = self.dyn_fcn_dx(x, q, pars).flatten()
+                out = np.atleast_2d(self.dyn_fcn_dx(x, q, pars).flatten())
             else:
                 out = self.dyn_fcn(x, q, pars)
         return out
@@ -58,25 +58,27 @@ class StateSpaceModel(object):
         if self.r_additive:
             assert len(xr) == self.xD
             if dx:
-                out = self.meas_fcn_dx(xr, 0, pars).flatten()
+                out = np.atleast_2d(self.meas_fcn_dx(xr, 0, pars).flatten())
             else:
                 out = self.meas_fcn(xr, 0, pars)
         else:
             assert len(xr) == self.xD + self.rD
             x, r = xr[:self.xD], xr[-self.rD:]
             if dx:
-                out = self.meas_fcn_dx(x, r, pars).flatten()
+                out = np.atleast_2d(self.meas_fcn_dx(x, r, pars).flatten())
             else:
                 out = self.meas_fcn(x, r, pars)
         return out
 
     def check_jacobians(self, eps=1e-16):
+        # FIXME: generate x depending on the noise additivity
         x = np.random.rand(self.xD)
         h = np.sqrt(eps)
         hdiag = np.diag(h * np.ones(self.xD))
         assert hdiag.shape == (self.xD, self.xD)
         xph, xmh = x[:, na] + hdiag, x[:, na] - hdiag
         par = np.atleast_1d(1.0)
+        par0 = np.atleast_1d(0.0)
         fph = np.zeros((self.xD, self.xD))
         hph = np.zeros((self.zD, self.xD))
         fmh, hmh = fph.copy(), hph.copy()
@@ -85,10 +87,10 @@ class StateSpaceModel(object):
             # fmh[:, i] = self.dyn_fcn(xmh[:, i], xmh[:, i], par)
             # hph[:, i] = self.meas_fcn(xph[:, i], xph[:, i], par)
             # hmh[:, i] = self.meas_fcn(xmh[:, i], xmh[:, i], par)
-            fph[:, i] = self.dyn_fcn(xph[:, i], 0.0, par)
-            fmh[:, i] = self.dyn_fcn(xmh[:, i], 0.0, par)
-            hph[:, i] = self.meas_fcn(xph[:, i], 0.0, par)
-            hmh[:, i] = self.meas_fcn(xmh[:, i], 0.0, par)
+            fph[:, i] = self.dyn_fcn(xph[:, i], par0, par)
+            fmh[:, i] = self.dyn_fcn(xmh[:, i], par0, par)
+            hph[:, i] = self.meas_fcn(xph[:, i], par0, par)
+            hmh[:, i] = self.meas_fcn(xmh[:, i], par0, par)
         jac_fx = (2 * h) ** -1 * (fph - fmh)
         jac_hx = (2 * h) ** -1 * (hph - hmh)
         print "Errors in Jacobians\n{}\n{}".format(np.abs(jac_fx - self.dyn_fcn_dx(x, x, par)),

models/ungm.py
@@ -86,20 +86,19 @@ class UNGMnonadd(StateSpaceModel):
         self.set_pars('r_cov', np.atleast_2d(r_cov))
 
     def dyn_fcn(self, x, q, pars):
-        return np.asarray([0.5 * x[0] + 25 * (x[0] / (1 + x[0] ** 2)) + 8 * np.cos(1.2 * q[0] * pars[0])])
+        return np.asarray([0.5 * x[0] * q[0] + 25 * (x[0] / (1 + x[0] ** 2)) + 8 * np.cos(1.2 * pars[0])])
 
     def meas_fcn(self, x, r, pars):
-        return np.asarray([0.05 * x[0] ** 2]) + 0.5 * r[0] * np.sin(r[0])
+        return np.asarray([0.05 * r[0] * x[0] ** 2])
 
     def par_fcn(self, time):
         return np.atleast_1d(time)
 
-    def dyn_fcn_dx(self, x, q, pars):
-        return np.asarray([0.5 + 25 * (1 - x[0] ** 2) / (1 + x[0] ** 2) ** 2,
-                           -8 * np.sin(1.2 * q[0] * pars[0]) * 1.2 * pars[0]])
+    def dyn_fcn_dx(self, x, q, pars):  # TODO: workout the Jacobians properly
+        return np.asarray([0.5 + 25 * (1 - x[0] ** 2) / (1 + x[0] ** 2) ** 2, 16 * q[0] * np.cos(1.2 * pars[0])])
 
     def meas_fcn_dx(self, x, r, pars):
-        return np.asarray([0.1 * x[0], 0.5 * (np.sin(r) + r * np.cos(r))])
+        return np.asarray([0.1 * x[0], r[0]])
 
 
 def ungm_demo():
@@ -129,11 +128,11 @@ def ungm_nonadd_demo():
 
 def ungm_filter_demo(filt_class, **kwargs):
     assert issubclass(filt_class, StateSpaceInference)
-    system = UNGM(q_cov=10, r_cov=1)
+    system = UNGM(q_cov=10, r_cov=1, x0_cov=5.0)
     # create filter object, pass in additional kwargs
     filt = filt_class(system, **kwargs)
     # simulate dynamic system for given number of steps and mc simulations
-    time_steps, mc = 250, 50
+    time_steps, mc = 500, 100
     x, z = system.simulate(time_steps, mc_sims=mc)
     print "Running {} filter/smoother ({} time steps, {} MC simulations) ...".format(filt_class.__name__,
                                                                                      time_steps, mc)

transforms/bayesquad.py
@@ -53,7 +53,7 @@ class GPQuad(MomentTransform):
         iLam2 = np.atleast_2d(np.diag(el ** -2))
 
         inp = self.unit_sp.T.dot(iLam1)  # sigmas / el[:, na] (x - m)^T*sqrt(Lambda^-1) # (numSP, xdim)
-        K = np.exp(2 * np.log(alpha) - 0.5 * self._maha(inp, inp))
+        K = np.exp(2 * np.log(alpha) - 0.5 * maha(inp, inp))
         iK = cho_solve(cho_factor(K + jitter * eye_n), eye_n)
         B = iLam2 + eye_d  # (D, D)
         c = alpha ** 2 / np.sqrt(det(B))
@@ -63,7 +63,7 @@ class GPQuad(MomentTransform):
         inp = inp.dot(iLam1)
         R = 2 * iLam2 + eye_d
         t = 1 / np.sqrt(det(R))
-        L = np.exp((zet[:, na] + zet[:, na].T) + self._maha(inp, -inp, V=0.5 * inv(R)))
+        L = np.exp((zet[:, na] + zet[:, na].T) + maha(inp, -inp, V=0.5 * inv(R)))
         q = c * l  # evaluations of the kernel mean map (from the viewpoint of RHKS methods)
         # mean weights
         wm_f = q.dot(iK)
@@ -165,23 +165,6 @@ class GPQuad(MomentTransform):
         # finds hypers by minimizing the sum of log-marginal likelihood and the integral variance objectives
         pass
 
-    @staticmethod
-    def _maha(x, y, V=None):
-        """
-        Pair-wise Mahalanobis distance of rows of x and y with given weight matrix V.
-        :param x: (n, d) matrix of row vectors
-        :param y: (n, d) matrix of row vectors
-        :param V: weight matrix (d, d), if V=None, V=eye(d) is used
-        :return:
-        """
-        if V is None:
-            V = np.eye(x.shape[1])
-        x2V = np.sum(x.dot(V) * x, 1)
-        y2V = np.sum(y.dot(V) * y, 1)
-        MD = (x2V[:, na] + y2V[:, na].T) - 2 * x.dot(V).dot(y.T)
-
-        return MD
-
 
 class GPQuadAlt(GPQuad):
     def apply(self, f, mean, cov, pars):
@@ -235,7 +218,7 @@ class TPQuad(MomentTransform):
         iLam2 = np.atleast_2d(np.diag(el ** -2))
 
         inp = self.unit_sp.T.dot(iLam1)  # sigmas / el[:, na] (x - m)^T*sqrt(Lambda^-1) # (numSP, xdim)
-        K = np.exp(2 * np.log(alpha) - 0.5 * self._maha(inp, inp))
+        K = np.exp(2 * np.log(alpha) - 0.5 * maha(inp, inp))
         iK = cho_solve(cho_factor(K + jitter * eye_n), eye_n)
         B = iLam2 + eye_d  # (D, D)
         c = alpha ** 2 / np.sqrt(det(B))
@@ -245,7 +228,7 @@ class TPQuad(MomentTransform):
         inp = inp.dot(iLam1)
         R = 2 * iLam2 + eye_d
         t = 1 / np.sqrt(det(R))
-        L = np.exp((zet[:, na] + zet[:, na].T) + self._maha(inp, -inp, V=0.5 * inv(R)))
+        L = np.exp((zet[:, na] + zet[:, na].T) + maha(inp, -inp, V=0.5 * inv(R)))
         q = c * l  # evaluations of the kernel mean map (from the viewpoint of RHKS methods)
         # mean weights
         wm_f = q.dot(iK)
@@ -262,19 +245,21 @@ class TPQuad(MomentTransform):
         self.model_var = np.diag((alpha ** 2 - np.trace(iKQ)) * np.ones((d, 1)))
         return wm_f, wc_f, wc_fx
 
-    @staticmethod
-    def _maha(x, y, V=None):
-        """
-        Pair-wise Mahalanobis distance of rows of x and y with given weight matrix V.
-        :param x: (n, d) matrix of row vectors
-        :param y: (n, d) matrix of row vectors
-        :param V: weight matrix (d, d), if V=None, V=eye(d) is used
-        :return:
-        """
-        if V is None:
-            V = np.eye(x.shape[1])
-        x2V = np.sum(x.dot(V) * x, 1)
-        y2V = np.sum(y.dot(V) * y, 1)
-        MD = (x2V[:, na] + y2V[:, na].T) - 2 * x.dot(V).dot(y.T)
 
-        return MD
+def maha(x, y, V=None):
+    """
+    Pair-wise Mahalanobis distance of rows of x and y with given weight matrix V.
+    :param x: (n, d) matrix of row vectors
+    :param y: (n, d) matrix of row vectors
+    :param V: weight matrix (d, d), if V=None, V=eye(d) is used
+    :return:
+    """
+    if V is None:
+        V = np.eye(x.shape[1])
+    x2V = np.sum(x.dot(V) * x, 1)
+    y2V = np.sum(y.dot(V) * y, 1)
+    return (x2V[:, na] + y2V[:, na].T) - 2 * x.dot(V).dot(y.T)
+
+# TODO: add GPQ+D(derivative observations),
+# TODO: add GPQ+TD (total derivative observations)
+# TODO: add GPQ+DIV (divergence observations)