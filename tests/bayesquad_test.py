6b34764ac6d3eb0c1917963a1bd30023ffd03769
Jakub Prüher <jacobnzw@gmail.com>
2016-07-28 13:28:52 +0200

    Tests corrected to reflect the redesigned BQ filters.

--- tests/bayesquad_test.py
+++ tests/bayesquad_test.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 import numpy as np
 import numpy.linalg as la
 
-from bayesquad import GPQ
+from transforms.bayesquad import GPQ
 from models.pendulum import Pendulum
 from models.ungm import UNGM
 
--- tests/ssmodel_test.py
+++ tests/ssmodel_test.py
@@ -2,25 +2,21 @@ import unittest
 
 import numpy as np
 
-from inference import *
+from inference.extended import ExtendedKalman
+from inference.unscented import UnscentedKalman
+from inference.cubature import CubatureKalman
+from inference.gausshermite import GaussHermiteKalman
+from inference.gpquad import GPQKalman
+from inference.tpquad import TPQKalman
 from models.pendulum import Pendulum
 from models.ungm import UNGM, UNGMnonadd
-from transforms import Unscented
-
-
-def default_bq_unit_sp(sys):
-    nq = sys.xD if sys.q_additive else sys.xD + sys.qD
-    nr = sys.xD if sys.r_additive else sys.xD + sys.rD
-    unit_sp_f = Unscented.unit_sigma_points(nq, np.sqrt(nq + 0))
-    unit_sp_h = Unscented.unit_sigma_points(nr, np.sqrt(nr + 0))
-    return unit_sp_f, unit_sp_h
 
 
 def default_bq_hypers(sys):
     nq = sys.xD if sys.q_additive else sys.xD + sys.qD
     nr = sys.xD if sys.r_additive else sys.xD + sys.rD
-    hypers_f = {'sig_var': 1.0, 'lengthscale': 3.0 * np.ones((nq,)), 'noise_var': 1e-8}
-    hypers_h = {'sig_var': 1.0, 'lengthscale': 3.0 * np.ones((nr,)), 'noise_var': 1e-8}
+    hypers_f = {'alpha': 1.0, 'el': 3.0 * np.ones((nq,))}
+    hypers_h = {'alpha': 1.0, 'el': 3.0 * np.ones((nr,))}
     return hypers_f, hypers_h
 
 
@@ -45,14 +41,13 @@ class TestUNGM(unittest.TestCase):
         ssm = UNGM()
         x, z = ssm.simulate(100, mc_sims=1)
         hyp_dyn, hyp_meas = default_bq_hypers(ssm)
-        usp_dyn, usp_meas = default_bq_unit_sp(ssm)
         inf_method = (
             ExtendedKalman(ssm),
             UnscentedKalman(ssm, kappa=0.0),
             CubatureKalman(ssm),
             GaussHermiteKalman(ssm),
-            GPQuadKalman(ssm, usp_dyn, usp_meas, hyp_dyn, hyp_meas),
-            TPQuadKalman(ssm, usp_dyn, usp_meas, hyp_dyn, hyp_meas),
+            GPQKalman(ssm, 'rbf', 'ut', hyp_dyn, hyp_meas),
+            TPQKalman(ssm, 'rbf', 'ut', hyp_dyn, hyp_meas),
         )
         for inf in inf_method:
             inf.forward_pass(z[..., 0])
@@ -65,14 +60,13 @@ class TestUNGM(unittest.TestCase):
         ssm = UNGMnonadd(x0_mean=0.1)
         x, z = ssm.simulate(100, mc_sims=1)
         hyp_dyn, hyp_meas = default_bq_hypers(ssm)
-        usp_dyn, usp_meas = default_bq_unit_sp(ssm)
         inf_method = (
             ExtendedKalman(ssm),
             UnscentedKalman(ssm),
             CubatureKalman(ssm),
             GaussHermiteKalman(ssm),
-            GPQuadKalman(ssm, usp_dyn, usp_meas, hyp_dyn, hyp_meas),
-            TPQuadKalman(ssm, usp_dyn, usp_meas, hyp_dyn, hyp_meas),
+            GPQKalman(ssm, 'rbf', 'ut', hyp_dyn, hyp_meas),
+            TPQKalman(ssm, 'rbf', 'ut', hyp_dyn, hyp_meas),
         )
         for inf in inf_method:
             print(r"Testing {} ...".format(inf.__class__.__name__), end=' ')
@@ -93,14 +87,13 @@ class TestPendulum(unittest.TestCase):
         ssm = Pendulum()
         x, z = ssm.simulate(100, mc_sims=1)
         hyp_dyn, hyp_meas = default_bq_hypers(ssm)
-        usp_dyn, usp_meas = default_bq_unit_sp(ssm)
         inf_method = (
             ExtendedKalman(ssm),
             UnscentedKalman(ssm),
             CubatureKalman(ssm),
             GaussHermiteKalman(ssm),
-            GPQuadKalman(ssm, usp_dyn, usp_meas, hyp_dyn, hyp_meas),
-            TPQuadKalman(ssm, usp_dyn, usp_meas, hyp_dyn, hyp_meas),
+            GPQKalman(ssm, 'rbf', 'ut', hyp_dyn, hyp_meas),
+            TPQKalman(ssm, 'rbf', 'ut', hyp_dyn, hyp_meas),
         )
         for inf in inf_method:
             inf.forward_pass(z[..., 0])
