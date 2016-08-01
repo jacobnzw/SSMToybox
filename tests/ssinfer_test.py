006c3b8babd4a9a156cc125962acbcbac23dc3a8
Jakub Pruher <jacobnzw@gmail.com>
2016-08-01 15:32:04 +0200

    Adding a test for algorithms based on MarginalInference.

---
+++ tests/ssinfer_test.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+import numpy as np
+import numpy.linalg as la
+
+from inference.ssinfer import MarginalInference
+
+# TODO: tests for filters derived from MarginalInference
