77a5c9fb3fc530cc4d71de82b58fb1994daec033
Jakub Pruher <jacobnzw@gmail.com>
2016-08-11 11:08:56 +0200

    KL-divergence and SKL added as metrics. Added docstrings.

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from numpy import newaxis as na
 import pandas as pd
 
 """
-Preliminary implementation of routines computing various performance metrics used in filtering.
+Preliminary implementation of routines computing various performance metrics used in state estimation.
 
 Every function expects data in a numpy array of shape (D, N, M, ...), where
 D - dimension, N - time steps, M - MC simulations, ... - other optional irrelevant dimensions.
@@ -14,11 +14,17 @@ def rmse(x, m):
     """
     Root Mean Squared Error
 
+    .. math::
+
+    \sqrt{\frac{1}{K}\sum\limits_{k=1}^{K}(x_k - m_k)^{\top}(x_k - m_k)}
+
     Parameters
     ----------
-    x numpy.ndarray of (d, time_steps, mc_sims)
+    x: numpy.ndarray with shape (d, time_steps, mc_sims)
         True state
-    m numpy.ndarray of (d, time_steps, mc_sims, algs)
+
+    m: numpy.ndarray with shape (d, time_steps, mc_sims, algs)
+        State mean
 
     Returns
     -------
@@ -32,11 +38,34 @@ def rmse(x, m):
 
 
 def nci(x, m, P):
+    """
+    Non-credibility index of the state estimate
+
+    .. math::
+
+        \frac{10}{K}\sum\limits_{k=1}^{K} \log_10 \frac{(x - m)^{\top}P^{-1}(x - m)}{(x - m)^{\top}\Sig^{-1}(x - m)},
+
+    where the :math:`\Sig` is the MSE matrix.
+
+    Parameters
+    ----------
+    x:
+        True state
+    m:
+        State mean
+    P:
+        State covariance
+
+    Returns
+    -------
+
+    """
     # dimension of state, # time steps, # MC simulations, # inference algorithms (filters/smoothers)
     d, time, mc_sims, algs = m.shape
     dx = x[..., na] - m
     # Mean Square Error matrix
     MSE = np.empty((d, d, time, mc_sims, algs))
+    # TODO: these loops can be moved to a general function which calls the specific metric
     for k in range(time):
         for s in range(mc_sims):
             for alg in range(algs):
@@ -59,6 +88,22 @@ def nci(x, m, P):
 
 
 def nll(x, m, P):
+    """
+    Negative log-likelihood of the state estimate given the true state.
+
+    Parameters
+    ----------
+    x:
+        True state
+    m:
+        State mean
+    P:
+        State covariance
+
+    Returns
+    -------
+
+    """
     d, time, mc_sims, algs = m.shape
     dx = x[..., na] - m
     NLL = np.empty((1, time, mc_sims, algs))
@@ -72,9 +117,70 @@ def nll(x, m, P):
     return NLL[:, 1:, ...].mean(axis=1)  # average over time steps (ignore the 1st)
 
 
+def kl(mean_0, cov_0, mean_1, cov_1):
+    """
+    KL-divergence
+
+    Parameters
+    ----------
+    mean_0
+    cov_0
+    mean_1
+    cov_1
+
+    Returns
+    -------
+    :float
+        KL-divergence of two Gaussian densities.
+    """
+    k = 1 if np.isscalar(mean_0) else mean_0.shape[0]
+    cov_0, cov_1 = np.atleast_2d(cov_0, cov_1)
+    dmu = mean_0 - mean_1
+    dmu = np.asarray(dmu)
+    det_0 = np.linalg.det(cov_0)
+    det_1 = np.linalg.det(cov_1)
+    inv_1 = np.linalg.inv(cov_1)
+    kl = 0.5 * (np.trace(np.dot(inv_1, cov_0)) + np.dot(dmu.T, inv_1).dot(dmu) + np.log(det_0 / det_1) - k)
+    return np.asscalar(kl)
+
+
+def skl(mean_0, cov_0, mean_1, cov_1):
+    """
+    Symmetrized KL-divergence :math:`0.5[KL(q(x)||p(x)) + KL(p(x)||q(x))]`
+
+    Parameters
+    ----------
+    mean_0
+    cov_0
+    mean_1
+    cov_1
+
+    Returns
+    -------
+    :float
+        Symmetrized KL-divergence
+    """
+    return 0.5 * (kl(mean_0, cov_0, mean_1, cov_1) + kl(mean_1, cov_1, mean_0, cov_0))
+
+
 def bootstrap_var(data, samples=1000):
-    # Estimates variance of a given data sample by bootstrapping
-    # data (1, mc_sims)
+    """
+    Estimates variance of a given data sample by bootstrapping
+
+    Parameters
+    ----------
+    data: numpy.ndarray (1, mc_sims)
+        Data set
+    samples: int
+        how many samples to use during bootstrapping
+
+    Returns
+    -------
+    : float
+        Bootstrap estimate of variance of the data set.
+    """
+    #
+    # data
     data = data.squeeze()
     mc_sims = data.shape[0]
     # sample with replacement to create new datasets
