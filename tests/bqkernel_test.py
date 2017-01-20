RBF kernel passed tests, had to fix eval and kernel "covariance" code.

tests/bqkernel_test.py
@@ -7,89 +7,82 @@ from transforms.bqkernel import RBF
 
 
 class RBFKernelTest(TestCase):
-    # TODO: could be made into general test class for arbitrary kernel
+
     @classmethod
     def setUpClass(cls):
-        cls.hypers_1d = {'alpha': 1.0, 'el': 3.0 * np.ones(1, )}
-        cls.hypers_2d = {'alpha': 1.0, 'el': 3.0 * np.ones(2, )}
-        cls.kern_rbf_1d = RBF(1, cls.hypers_1d)
-        cls.kern_rbf_2d = RBF(2, cls.hypers_2d)
+        cls.par_1d = np.array([[1, 3]])
+        cls.par_2d = np.array([[1, 3, 3]])
+        cls.kern_rbf_1d = RBF(1, cls.par_1d)
+        cls.kern_rbf_2d = RBF(2, cls.par_2d)
         cls.data_1d = np.array([[1, -1, 0]], dtype=float)
         cls.data_2d = np.hstack((np.zeros((2, 1)), np.eye(2), -np.eye(2)))
         cls.test_data_1d = np.atleast_2d(np.linspace(-5, 5, 50))
         cls.test_data_2d = np.random.multivariate_normal(np.zeros((2,)), np.eye(2), 50).T
 
     def test_eval(self):
+
         # check dimension, shape, symmetry and positive definiteness
-        K = self.kern_rbf_1d.eval(self.data_1d)
+        K = self.kern_rbf_1d.eval(self.par_1d, self.data_1d)
         self.assertTrue(K.ndim == 2)
         self.assertTrue(K.shape == (3, 3))
         self.assertTrue(np.array_equal(K, K.T))
         la.cholesky(K)
+
         # higher-dimensional inputs
-        K = self.kern_rbf_2d.eval(self.data_2d)
+        K = self.kern_rbf_2d.eval(self.par_2d, self.data_2d)
         self.assertTrue(K.ndim == 2)
         self.assertTrue(K.shape == (5, 5))
         self.assertTrue(np.array_equal(K, K.T))
         la.cholesky(K)
-        # kx, kxx
-        kx = self.kern_rbf_1d.eval(self.test_data_1d, self.data_1d)
-        kxx = self.kern_rbf_1d.eval(self.test_data_1d, self.test_data_1d)
-        kxx_diag = self.kern_rbf_1d.eval(self.test_data_1d, self.test_data_1d, diag=True)
+
+        # check computation of cross-covariances kx, kxx
+        kx = self.kern_rbf_1d.eval(self.par_1d, self.test_data_1d, self.data_1d)
+        kxx = self.kern_rbf_1d.eval(self.par_1d, self.test_data_1d, self.test_data_1d)
+        kxx_diag = self.kern_rbf_1d.eval(self.par_1d, self.test_data_1d, self.test_data_1d, diag=True)
         self.assertTrue(kx.shape == (50, 3))
         self.assertTrue(kxx.shape == (50, 50))
         self.assertTrue(kxx_diag.shape == (50,))
-        kx = self.kern_rbf_2d.eval(self.test_data_2d, self.data_2d)
-        kxx = self.kern_rbf_2d.eval(self.test_data_2d, self.test_data_2d)
-        kxx_diag = self.kern_rbf_2d.eval(self.test_data_2d, self.test_data_2d, diag=True)
+
+        kx = self.kern_rbf_2d.eval(self.par_2d, self.test_data_2d, self.data_2d)
+        kxx = self.kern_rbf_2d.eval(self.par_2d, self.test_data_2d, self.test_data_2d)
+        kxx_diag = self.kern_rbf_2d.eval(self.par_2d, self.test_data_2d, self.test_data_2d, diag=True)
         self.assertTrue(kx.shape == (50, 5))
         self.assertTrue(kxx.shape == (50, 50))
         self.assertTrue(kxx_diag.shape == (50,))
 
     def test_exp_x_kx(self):
-        q = self.kern_rbf_1d.exp_x_kx(self.data_1d)
+
+        q = self.kern_rbf_1d.exp_x_kx(self.data_1d, self.par_1d)
         self.assertTrue(q.shape == (3,))
         self.assertTrue(np.alltrue(q >= 0))
-        q = self.kern_rbf_2d.exp_x_kx(self.data_2d)
+
+        q = self.kern_rbf_2d.exp_x_kx(self.data_2d, self.par_2d)
         self.assertTrue(q.shape == (5,))
         self.assertTrue(np.alltrue(q >= 0))
 
     def test_exp_x_kxx(self):
-        self.kern_rbf_1d.exp_x_kxx()
-        self.kern_rbf_2d.exp_x_kxx()
+        self.kern_rbf_1d.exp_x_kxx(self.par_1d)
+        self.kern_rbf_2d.exp_x_kxx(self.par_2d)
 
     def test_exp_xy_kxy(self):
-        self.kern_rbf_1d.exp_xy_kxy()
-        self.kern_rbf_2d.exp_xy_kxy()
+        self.kern_rbf_1d.exp_xy_kxy(self.par_1d)
+        self.kern_rbf_2d.exp_xy_kxy(self.par_2d)
 
     def test_exp_x_xkx(self):
-        r = self.kern_rbf_1d.exp_x_xkx(self.data_1d)
+        r = self.kern_rbf_1d.exp_x_xkx(self.data_1d, self.par_1d)
         self.assertTrue(r.shape == (1, 3))
-        r = self.kern_rbf_2d.exp_x_xkx(self.data_2d)
+
+        r = self.kern_rbf_2d.exp_x_xkx(self.data_2d, self.par_2d)
         self.assertTrue(r.shape == (2, 5))
 
     def test_exp_x_kxkx(self):
-        q = self.kern_rbf_1d.exp_x_kxkx(self.data_1d)
+        q = self.kern_rbf_1d.exp_x_kxkx(self.data_1d, self.par_1d, self.par_1d)
         self.assertTrue(q.shape == (3, 3))
-        self.assertTrue(np.array_equal(q, q.T))
+        self.assertTrue(np.array_equal(q, q.T), 'Result not symmetric.')
         la.cholesky(q)
-        q = self.kern_rbf_2d.exp_x_kxkx(self.data_2d)
+
+        q = self.kern_rbf_2d.exp_x_kxkx(self.data_2d, self.par_2d, self.par_2d)
         self.assertTrue(q.shape == (5, 5))
-        self.assertTrue(np.array_equal(q, q.T))
+        self.assertTrue(np.array_equal(q, q.T), 'Result not symmetric.')
         la.cholesky(q)
 
-    def test_exp_model_variance(self):
-        emv = self.kern_rbf_1d.exp_model_variance(self.data_1d)
-        self.assertTrue(emv >= 0)
-        emv = self.kern_rbf_2d.exp_model_variance(self.data_2d)
-        self.assertTrue(emv >= 0)
-
-    def test_integral_variance(self):
-        ivar = self.kern_rbf_1d.integral_variance(self.data_1d)
-        self.assertTrue(ivar >= 0)
-        ivar = self.kern_rbf_2d.integral_variance(self.data_2d)
-        self.assertTrue(ivar >= 0)
-
-# class AffineKernelTest(TestCase):
-#     def test_eval(self):
-#         pass

transforms/bqkernel.py
@@ -202,7 +202,7 @@ class RBF(Kernel):
         Radial Basis Function kernel.
 
         .. math::
-           k(x, x') = s^2 \exp(-\frac{1}{2}(x - x')^{\top}\ Lambda^{-1} (x - x') \right)
+           k(x, x') = s^2 \exp\left(-\frac{1}{2}(x - x')^{\top}\ Lambda^{-1} (x - x') \right)
 
         Parameters
         ----------
@@ -231,7 +231,7 @@ class RBF(Kernel):
             x2 = x1.copy()
 
         alpha, sqrt_inv_lam = RBF._unpack_parameters(hyp)
-        alpha = 1.0 if scaling else alpha
+        alpha = 1.0 if not scaling else alpha
 
         x1 = sqrt_inv_lam.dot(x1)
         x2 = sqrt_inv_lam.dot(x2)
@@ -242,11 +242,11 @@ class RBF(Kernel):
         else:
             return np.exp(2 * np.log(alpha) - 0.5 * self._maha(x1.T, x2.T))
 
-    def exp_x_kx(self, x, hyp, ignore_alpha=True):
+    def exp_x_kx(self, x, hyp, scaling=False):
         # a.k.a. kernel mean map w.r.t. standard Gaussian PDF
         # hyp (D+1,) array_like
         alpha, sqrt_inv_lam = RBF._unpack_parameters(hyp)
-        alpha = 1.0 if ignore_alpha else alpha
+        alpha = 1.0 if not scaling else alpha
 
         inv_lam = sqrt_inv_lam ** 2
         lam = np.diag(inv_lam.diagonal() ** -1)
@@ -272,7 +272,7 @@ class RBF(Kernel):
         inv_lam = sqrt_inv_lam ** 2
         return alpha ** 2 * la.det(2 * inv_lam + self.eye_d) ** -0.5
 
-    def exp_x_kxkx(self, x, hyp, hyp_1, ignore_alpha=True):
+    def exp_x_kxkx(self, x, hyp, hyp_1, scaling=False):
         """
         "Correlation" matrix of kernels with elements
 
@@ -286,16 +286,18 @@ class RBF(Kernel):
         x : numpy.ndarray of shape (D, N)
         hyp : numpy.ndarray of shape (D, )
         hyp_1 : numpy.ndarray of shape (D, )
-        ignore_alpha : bool
+        scaling : bool
 
 
         Returns
         -------
 
         """
+
+        # unpack kernel parameters
         alpha, sqrt_inv_lam = RBF._unpack_parameters(hyp)
         alpha_1, sqrt_inv_lam_1 = RBF._unpack_parameters(hyp_1)
-        alpha, alpha_1 = (1.0, 1.0) if ignore_alpha else alpha, alpha_1
+        alpha, alpha_1 = (1.0, 1.0) if not scaling else (alpha, alpha_1)
         inv_lam = sqrt_inv_lam ** 2
         inv_lam_1 = sqrt_inv_lam_1 ** 2
 
@@ -308,13 +310,13 @@ class RBF(Kernel):
         xi_1 = 2 * np.log(alpha_1) - 0.5 * np.sum(xi_1 * xi_1, axis=0)  # (N, )
 
         # \Lambda^{-1} * x
-        x = inv_lam.dot(x)  # (D, N)
+        x_0 = inv_lam.dot(x)  # (D, N)
         x_1 = inv_lam_1.dot(x)
 
         # R^{-1} = (\Lambda_m^{-1} + \Lambda_n^{-1} + \eye)^{-1}
         r = inv_lam + inv_lam_1 + self.eye_d  # (D, D)
 
-        n = (xi[:, na] + xi_1[na, :]) + 0.5 * self._maha(x.T, -x_1.T, V=la.inv(r))  # (N, N)
+        n = (xi[:, na] + xi_1[na, :]) + 0.5 * self._maha(x_0.T, -x_1.T, V=la.inv(r))  # (N, N)
         return la.det(r) ** -0.5 * np.exp(n)
 
     def der_hyp(self, x, hyp0):  # K as kwarg would save computation (would have to be evaluated w/ hyp0)
@@ -330,7 +332,19 @@ class RBF(Kernel):
 
     @staticmethod
     def _unpack_parameters(param):
-        # divide kernel parameters into kernel scaling and square-root of inverse lengthscale matrix
+        """
+        Extract scaling parameter and square-root of inverse lengthscale matrix from vector of kernel parameters.
+
+        Parameters
+        ----------
+        param : numpy.ndarray
+
+        Returns
+        -------
+        : tuple
+
+        """
+        param = param.astype(float).squeeze()
         return param[0], np.diag(param[1:] ** -1)
 
     def get_hyperparameters(self, hyp=None):