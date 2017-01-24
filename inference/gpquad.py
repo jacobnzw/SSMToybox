Fixed computation of covariance weights. Tests show major difference between GPQ and GPQMO covariances.

inference/gpquad.py
@@ -96,14 +96,12 @@ def main():
     dim = 5
     hdyn = np.array([[1, 25, 25, 25, 25, 25]], dtype=float)
     hobs = np.array([[1, 25, 25, 1e4, 1e4, 1e4]], dtype=float)
-    reentry_filter_demo(GPQKalman, hdyn, hobs, 'rbf', 'ut')
-
-    # used kernel parameters in table for nicer printing
     dyn_par_table = pd.DataFrame(hdyn, columns=['alpha'] + ['el_' + str(d) for d in range(1, dim+1)])
     obs_par_table = pd.DataFrame(hobs, columns=['alpha'] + ['el_' + str(d) for d in range(1, dim+1)])
     print('Kernel parameters used')
     print(dyn_par_table)
     print(obs_par_table)
+    reentry_filter_demo(GPQKalman, hdyn, hobs, 'rbf', 'ut')
 
     # multi-output GPQ Kalman
     null, lin = 1e4, 3

tests/bayesquad_test.py
@@ -6,6 +6,7 @@ import numpy.linalg as la
 from transforms.bayesquad import GPQ, GPQMO
 from models.pendulum import Pendulum
 from models.ungm import UNGM
+from models.tracking import ReentryRadar
 
 np.set_printoptions(precision=4)
 
@@ -121,24 +122,25 @@ class GPQMOTest(TestCase):
 
     def test_single_vs_multi_output(self):
         # results of the GPQ and GPQMO should be same if parameters properly chosen, GPQ is a special case of GPQMO
-        ssm = Pendulum()
+        ssm = ReentryRadar()
         f = ssm.dyn_eval
         dim_in, dim_out = ssm.xD, ssm.xD
 
         # input mean and covariance
-        mean_in, cov_in = np.zeros(dim_in, ), np.eye(dim_in)
+        mean_in, cov_in = ssm.pars['x0_mean'], ssm.pars['x0_cov']
 
         # single-output GPQ
-        ker_par_so = np.hstack((np.ones((1, 1)), 3 * np.ones((1, dim_in))))
+        ker_par_so = np.hstack((np.ones((1, 1)), 25 * np.ones((1, dim_in))))
         tf_so = GPQ(dim_in, ker_par_so)
 
         # multi-output GPQ
-        ker_par_mo = np.hstack((np.ones((dim_out, 1)), 3 * np.ones((dim_out, dim_in))))
+        ker_par_mo = np.hstack((np.ones((dim_out, 1)), 25 * np.ones((dim_out, dim_in))))
         tf_mo = GPQMO(dim_in, dim_out, ker_par_mo)
 
         # transformed moments
-        mean_so, cov_so, ccov_so = tf_so.apply(f, mean_in, cov_in, np.atleast_1d(1.0))
-        mean_mo, cov_mo, ccov_mo = tf_mo.apply(f, mean_in, cov_in, np.atleast_1d(1.0))
+        # FIXME: transformed covariances different
+        mean_so, cov_so, ccov_so = tf_so.apply(f, mean_in, cov_in, ssm.par_fcn(0))
+        mean_mo, cov_mo, ccov_mo = tf_mo.apply(f, mean_in, cov_in, ssm.par_fcn(0))
 
         # results of GPQ and GPQMO should be the same
         self.assertTrue(np.allclose(mean_so, mean_mo))