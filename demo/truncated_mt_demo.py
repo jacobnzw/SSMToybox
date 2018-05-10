a6c89e1dae260a272ec8c39a76cfd374ea7a68f9
Jakub Prüher <jacobnzw@gmail.com>
2018-05-10 12:05:43 +0200

    Truncated demo improved: SKL plot, support for full input matrix

--- demo/truncated_mt_demo.py
+++ demo/truncated_mt_demo.py
@@ -2,7 +2,7 @@ from mtran import MonteCarlo, Unscented, UnscentedTrunc
 import numpy as np
 import matplotlib.pyplot as plt
 from mtran import MomentTransform
-from utils import ellipse_points
+from utils import ellipse_points, symmetrized_kl_divergence
 
 
 def cartesian2polar(x, pars, dx=False):
@@ -13,7 +13,7 @@ def polar2cartesian(x, pars, dx=False):
     return x[0] * np.array([np.cos(x[1]), np.sin(x[1])])
 
 
-def mt_trunc_demo(mt_trunc, mt, dim=None, **kwargs):
+def mt_trunc_demo(mt_trunc, mt, dim=None, full_input_cov=True, **kwargs):
     """
     Comparison of truncated MT and vanilla MT on polar2cartesian transform for increasing state dimensions. The
     truncated MT is aware of the effective dimension, so we expect it to be closer to the true covariance
@@ -29,6 +29,8 @@ def mt_trunc_demo(mt_trunc, mt, dim=None, **kwargs):
     mt_trunc
     mt
     dim
+    full_input_cov : boolean
+        If `False`, a diagonal input covariance is used, otherwise a full covariance is used.
     kwargs
 
     Returns
@@ -42,14 +44,19 @@ def mt_trunc_demo(mt_trunc, mt, dim=None, **kwargs):
     dim = [2, 3, 4, 5] if dim is None else dim
     d_eff = 2
 
-    # observation model
+    # nonlinear integrand
     f = polar2cartesian
 
-    # use MC transform with lots of samples to compute the true transformed moments
+    # input mean and covariance
     mean_eff, cov_eff = np.array([1, np.pi / 2]), np.diag([0.05 ** 2, (np.pi / 10) ** 2])
+
+    if full_input_cov:
+        A = np.random.rand(d_eff, d_eff)
+        cov_eff = A.dot(cov_eff).dot(A.T)
+
+    # use MC transform with lots of samples to compute the true transformed moments
     tmc = MonteCarlo(d_eff, n=1e4)
     M_mc, C_mc, cc_mc = tmc.apply(f, mean_eff, cov_eff, None)
-
     # transformed samples
     x = np.random.multivariate_normal(mean_eff, cov_eff, size=int(1e3)).T
     fx = np.apply_along_axis(f, 0, x, None)
@@ -66,7 +73,7 @@ def mt_trunc_demo(mt_trunc, mt, dim=None, **kwargs):
         mean, cov = np.zeros(d), np.eye(d)
         mean[:d_eff], cov[:d_eff, :d_eff] = mean_eff, cov_eff
 
-        # transfored moments (w/o cross-covariance)
+        # transformed moments (w/o cross-covariance)
         M[:, i, 0], C[..., i, 0], cc = t.apply(f, mean, cov, None)
         M[:, i, 1], C[..., i, 1], cc = s.apply(f, mean, cov, None)
 
@@ -75,23 +82,35 @@ def mt_trunc_demo(mt_trunc, mt, dim=None, **kwargs):
         X[..., i, 1] = ellipse_points(M[:, i, 1], C[..., i, 1])
 
     # PLOTS: transformed samples, MC mean and covariance ground truth
-    plt.figure()
-    plt.plot(fx[0, :], fx[1, :], 'k.', alpha=0.15)
-    plt.plot(M_mc[0], M_mc[1], 'ro', markersize=6, lw=2)
-    plt.plot(X_mc[0, :], X_mc[1, :], 'r--', lw=2, label='MC')
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(fx[0, :], fx[1, :], 'k.', alpha=0.15)
+    ax[0].plot(M_mc[0], M_mc[1], 'ro', markersize=6, lw=2)
+    ax[0].plot(X_mc[0, :], X_mc[1, :], 'r--', lw=2, label='MC')
 
     # SR and SR-T mean and covariance for various state dimensions
     # TODO: it's more effective to plot SKL between the transformed and baseline covariances.
     for i, d in enumerate(dim):
-        plt.plot(M[0, i, 0], M[1, i, 0], 'b+', markersize=10, lw=2)
-        plt.plot(X[0, :, i, 0], X[1, :, i, 0], color='b', label='mt-trunc (d={})'.format(d))
+        ax[0].plot(M[0, i, 0], M[1, i, 0], 'b+', markersize=10, lw=2)
+        ax[0].plot(X[0, :, i, 0], X[1, :, i, 0], color='b', label='mt-trunc (d={})'.format(d))
     for i, d in enumerate(dim):
-        plt.plot(M[0, i, 0], M[1, i, 0], 'go', markersize=6)
-        plt.plot(X[0, :, i, 1], X[1, :, i, 1], color='g', label='mt (d={})'.format(d))
-    plt.axes().set_aspect('equal')
+        ax[0].plot(M[0, i, 1], M[1, i, 1], 'go', markersize=6)
+        ax[0].plot(X[0, :, i, 1], X[1, :, i, 1], color='g', label='mt (d={})'.format(d))
+    ax[0].set_aspect('equal')
     plt.legend()
+
+    # symmetrized KL-divergence
+    skl = np.zeros((len(dim), 2))
+    for i, d in enumerate(dim):
+        skl[i, 0] = symmetrized_kl_divergence(M_mc, C_mc, M[:, i, 0], C[..., i, 0])
+        skl[i, 1] = symmetrized_kl_divergence(M_mc, C_mc, M[:, i, 1], C[..., i, 1])
+    plt_opt = {'lw': 2, 'marker': 'o'}
+    ax[1].plot(dim, skl[:, 0], label='truncated', **plt_opt)
+    ax[1].plot(dim, skl[:, 1], label='original', **plt_opt)
+    ax[1].set_xticks(dim)
+    ax[1].set_xlabel('Dimension')
+    ax[1].set_ylabel('SKL')
     plt.show()
 
 
 if __name__ == '__main__':
-    mt_trunc_demo(UnscentedTrunc, Unscented, [2, 5, 10, 15], kappa=0)
+    mt_trunc_demo(UnscentedTrunc, Unscented, [2, 5, 10, 15], kappa=0, full_input_cov=True)
