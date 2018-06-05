be23c999bf1cbaa69ee362729aa82966e8d3281b
Jakub Pruher <jacobnzw@gmail.com>
2018-06-05 16:28:05 +0200

    NLML plotting added. The norm of the difference between the non-parametric kernel matrix estimate and the parametrized kernel matrix seems like a better objective, as suggested by visual inspection of the surface plots.

--- exper/kernel_fitting.py
+++ exper/kernel_fitting.py
@@ -133,7 +133,7 @@ def kernel_fit_nlml_sgd():
     # define GP model with num_dim inputs
     from bq.bqmod import GaussianProcess
     num_dim = 2
-    gp = GaussianProcess(num_dim, np.array([[1.0] + num_dim*[1.0]]), points='gh', point_par={'degree': 3})
+    gp = GaussianProcess(num_dim, np.array([[1.0] + num_dim*[10.0]]), points='gh', point_par={'degree': 3})
     x_samples = perturb_template(gp.points, samples=100, std=1.0)
     # x_samples = np.reshape(x_samples, (x_samples.shape[0], -1))
 
@@ -154,15 +154,21 @@ def kernel_fit_nlml_sgd():
     # Stochastic Gradient Descent: compute gradient using mini-batches
     log_theta = np.log(gp.kernel.par)  # initial guess of kernel parameters
     alpha = 0.001  # learning rate
-    num_iter = 20  # number of iterations
+    beta_1, beta_2 = 0.9, 0.999
+    m, v = np.zeros((num_dim, )), np.zeros((num_dim, ))
+    num_iter = 1  # number of iterations
     In = np.eye(x_samples.shape[-1])
     for i in range(num_iter):
         for x_batch, y_batch in zip(x_samples, fcn_evals):
+            print(np.exp(log_theta))
             _, grad = gp.neg_log_marginal_likelihood(log_theta, y_batch[:, None], x_batch, 1e-8*In)
             # fixed kernel scaling update
             log_theta[0, 1:] = log_theta[0, 1:] - alpha * grad[1:]
             # log_theta = log_theta - alpha * grad
-            # TODO: ADAM update
+            # ADAM update
+            # m = (beta_1 * m + (1 - beta_1) * grad[1:]) / (1 - beta_1**(i+1))
+            # v = (beta_2 * v + (1 - beta_2) * grad[1:] ** 2) / (1 - beta_2**(i+1))
+            # log_theta[0, 1:] = log_theta[0, 1:] - alpha * (m / (np.sqrt(v) + 10e-8))
 
         if i % 10 == 0:
             nlml = []
@@ -174,6 +180,73 @@ def kernel_fit_nlml_sgd():
     print(np.exp(log_theta))
 
 
+def plot_nlml():
+    # define GP model with num_dim inputs
+    from bq.bqmod import GaussianProcess
+    num_dim = 2
+    gp = GaussianProcess(num_dim, np.array([[1.0] + num_dim * [1.0]]), points='gh', point_par={'degree': 5})
+    x_samples = perturb_template(gp.points, samples=100, std=1.0)
+
+    # transpose from (D, N, num_samples) to (num_samples, D, N)
+    # x_samples = np.transpose(x_samples, axes=(2, 0, 1))
+
+    # unknown function (integrand); x is (D,)
+    f = lambda x: x.T.dot(x)
+    f = lambda x: np.arctan2(x[1], x[0])
+    f = lambda x: np.sum(x)
+    # f = lambda x: np.sqrt(x[0]**2 + x[1]**2)
+
+    # evaluate function (integrand) at every instance of perturbed point-set
+    fcn_evals = []
+    for x_batch in np.transpose(x_samples, axes=(2, 0, 1)):
+        fcn_evals.append(np.apply_along_axis(f, 0, x_batch))
+
+    fcn_evals = np.array(fcn_evals)
+    x_samples = np.reshape(x_samples, (x_samples.shape[0], -1))
+    In = np.eye(len(fcn_evals))
+
+    def kernel_obj(log_theta, x, K_true):
+        """
+        Kernel fitting objective.
+
+        Parameters
+        ----------
+        log_theta
+        x
+        K_true
+
+        Returns
+        -------
+
+        """
+        K_theta = gp.kernel.eval(np.exp(log_theta), x)
+        return np.linalg.norm(K_true - K_theta, ord=np.inf)
+        # return np.linalg.norm(K_true - K_theta, ord=np.inf) + np.linalg.det(K_true - K_theta)
+
+    # y = np.apply_along_axis(f, 0, gp.points)
+    # In = np.eye(len(y))
+    def nlml_wrapper(theta):
+        value, grad = gp.neg_log_marginal_likelihood(theta, fcn_evals.ravel()[:, None],
+                                                     np.reshape(x_samples, (x_samples.shape[0], -1)), 1e-8*In)
+        # value, grad = gp.neg_log_marginal_likelihood(theta, y[:, None], gp.points, 1e-8 * In)
+        return value
+
+    el = np.linspace(0.01, 10, 50)
+    EL_1, EL_2 = np.meshgrid(el, el)
+    # z = [nlml_wrapper(np.log(np.array([[1.0, el1, el2]]))) for el1, el2 in zip(EL_1.ravel(), EL_2.ravel())]
+    K_true = estimate_kernel_mat(fcn_evals.T)
+    z = [kernel_obj(np.log(np.array([[1.0, el1, el2]])), gp.points, K_true)
+         for el1, el2 in zip(EL_1.ravel(), EL_2.ravel())]
+    Z = np.asarray(z).reshape(EL_1.shape)
+
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(EL_1, EL_2, Z)
+    ax.set_xlabel('$\ell_1$')
+    ax.set_ylabel('$\ell_2$')
+    plt.show()
+
 
 if __name__ == '__main__':
     # from mtran import Unscented
@@ -184,4 +257,6 @@ if __name__ == '__main__':
     # plt.plot(x_2d_samples[0, :], x_2d_samples[1, :], 'ko', ms=8, alpha=0.2)
     # plt.show()
 
-    kernel_fit_nlml_sgd()
+    # kernel_fit_nlml_sgd()
+
+    plot_nlml()
