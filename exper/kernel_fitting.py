80ab696f9e7261ddb79167d7b616161684d13eec
Jakub Pruher <jacobnzw@gmail.com>
2018-06-05 12:12:22 +0200

    Stochastic gradient descent coded; seems to be stuck in local minima;  will try implementing ADAM update.

--- exper/kernel_fitting.py
+++ exper/kernel_fitting.py
@@ -129,11 +129,59 @@ def kernel_fit_demo_single():
     model.plot_model(xtest, y, theta_star, ytest)
 
 
+def kernel_fit_nlml_sgd():
+    # define GP model with num_dim inputs
+    from bq.bqmod import GaussianProcess
+    num_dim = 2
+    gp = GaussianProcess(num_dim, np.array([[1.0] + num_dim*[1.0]]), points='gh', point_par={'degree': 3})
+    x_samples = perturb_template(gp.points, samples=100, std=1.0)
+    # x_samples = np.reshape(x_samples, (x_samples.shape[0], -1))
+
+    # transpose from (D, N, num_samples) to (num_samples, D, N)
+    x_samples = np.transpose(x_samples, axes=(2, 0, 1))
+
+    # unknown function (integrand); x is (D,)
+    f = lambda x: x.T.dot(x)
+
+    # evaluate function (integrand) at every instance of perturbed point-set
+    fcn_evals = []
+    for x_batch in x_samples:
+        fcn_evals.append(np.apply_along_axis(f, 0, x_batch))
+    fcn_evals = np.array(fcn_evals)
+
+    y = np.apply_along_axis(f, 0, gp.points)
+
+    # Stochastic Gradient Descent: compute gradient using mini-batches
+    log_theta = np.log(gp.kernel.par)  # initial guess of kernel parameters
+    alpha = 0.001  # learning rate
+    num_iter = 20  # number of iterations
+    In = np.eye(x_samples.shape[-1])
+    for i in range(num_iter):
+        for x_batch, y_batch in zip(x_samples, fcn_evals):
+            _, grad = gp.neg_log_marginal_likelihood(log_theta, y_batch[:, None], x_batch, 1e-8*In)
+            # fixed kernel scaling update
+            log_theta[0, 1:] = log_theta[0, 1:] - alpha * grad[1:]
+            # log_theta = log_theta - alpha * grad
+            # TODO: ADAM update
+
+        if i % 10 == 0:
+            nlml = []
+            for x_batch, y_batch in zip(x_samples, fcn_evals):
+                nlml_iter, _ = gp.neg_log_marginal_likelihood(log_theta, y_batch[:, None], x_batch, gp.kernel.jitter)
+                nlml.append(nlml_iter)
+            print('{:d}, {:.4f}, {}'.format(i, sum(nlml), np.exp(log_theta)))
+
+    print(np.exp(log_theta))
+
+
+
 if __name__ == '__main__':
-    from mtran import Unscented
-    x_2d = Unscented.unit_sigma_points(2)
-    x_2d_samples = perturb_template(x_2d, samples=50, std=1.0)
-    x_2d_samples = np.reshape(x_2d_samples, (x_2d_samples.shape[0], -1))
-    plt.figure()
-    plt.plot(x_2d_samples[0, :], x_2d_samples[1, :], 'ko', ms=8, alpha=0.2)
-    plt.show()
+    # from mtran import Unscented
+    # x_2d = Unscented.unit_sigma_points(2)
+    # x_2d_samples = perturb_template(x_2d, samples=50, std=1.0)
+    # x_2d_samples = np.reshape(x_2d_samples, (x_2d_samples.shape[0], -1))
+    # plt.figure()
+    # plt.plot(x_2d_samples[0, :], x_2d_samples[1, :], 'ko', ms=8, alpha=0.2)
+    # plt.show()
+
+    kernel_fit_nlml_sgd()
