cfc9fb1fbe2f7599548921fc3ae389bb059e19d6
Jakub Pruher <jacobnzw@gmail.com>
2018-07-20 15:52:22 +0200

    Coordinated turn radar demo added.

--- bsq_tracking.py
+++ bsq_tracking.py
@@ -6,7 +6,8 @@ from collections import OrderedDict
 import time
 
 from ssmtoybox.ssinf import GaussianProcessKalman, BayesSardKalman, UnscentedKalman
-from ssmtoybox.ssmod import ReentryVehicleRadarTrackingSimpleGaussSSM, ReentryVehicleRadarTrackingGaussSSM
+from ssmtoybox.ssmod import ReentryVehicleRadarTrackingSimpleGaussSSM, ReentryVehicleRadarTrackingGaussSSM, \
+    CoordinatedTurnBearingsOnlyTrackingGaussSSM
 from ssmtoybox.dynsys import ReentryVehicleRadarTrackingSimpleGaussSystem, ReentryVehicleRadarTrackingGaussSystem
 from ssmtoybox.utils import mse_matrix, squared_error, log_cred_ratio
 
@@ -335,6 +336,136 @@ def reentry_demo():
     plt.show()
 
 
+def coordinated_turn_radar_demo():
+    num_steps = 100
+    mc_sims = 20
+
+    # sensor positions
+    x_min, x_max = -10000, 10000
+    y_min, y_max = -10000, 10000
+    S = np.array([[x_min, y_min],
+                  [x_min, y_max],
+                  [x_max, y_min],
+                  [x_max, y_max]])
+    tau = 1.0
+    # generate data
+    sys = CoordinatedTurnBearingsOnlyTrackingGaussSSM(dt=tau, sensor_pos=S)
+    x, y = sys.simulate(num_steps, mc_sims)
+
+    # weed out trajectories venturing outside of the sensor rectangle
+    ix = np.all(np.abs(x[(0, 2), ...]) <= x_max, axis=(0, 1))
+    x, y = x[..., ix], y[..., ix]
+    print('{:.2f}% of trajectories weeded out.'.format(100 * np.count_nonzero(ix == False) / len(ix)))
+
+    # state-space model for filters
+    ssm = CoordinatedTurnBearingsOnlyTrackingGaussSSM(dt=tau, sensor_pos=S)
+    # Initialize filters
+    par_dyn = np.array([[1.0, 1, 1, 1, 1, 1]])
+    par_obs = np.array([[1.0, 0.9, 0.9, 1e4, 1e4, 1e4]])
+    mul_ut = np.hstack((np.zeros((ssm.xD, 1)), np.eye(ssm.xD), 2 * np.eye(ssm.xD))).astype(np.int)
+    alg = OrderedDict({
+        # GaussianProcessKalman(ssm, par_dyn, par_obs, kernel='rbf', points='ut'),
+        'bsqkf': BayesSardKalman(ssm, par_dyn, par_obs, mul_ut, mul_ut, points='ut'),
+        'ukf': UnscentedKalman(ssm, beta=0),
+    })
+
+    kpdyn = np.array([[1.0, 1, 1e3, 1, 1e3, 1e3],
+                      [1.0, 1e3, 1, 1e3, 1, 1e3],
+                      [1.0, 3, 3, 3, 3, 1],
+                      [1.0, 3, 3, 3, 3, 1],
+                      [1.0, 1e3, 1e3, 1e3, 1e3, 1]])
+    alg['bsqkf'].tf_dyn.model.model_var = 1e-3*np.eye(5)  # multivariate_emv(alg['bsqkf'].tf_dyn, kpdyn, mul_ut)
+    kpobs = np.array([[1.0, 1, 1, 1e2, 1e2, 1e2],
+                      [1.0, 1.4, 1.4, 1e2, 1e2, 1e2]])
+    # multivariate_emv(alg[0].tf_meas, kpobs, mul_ut)  # 1e-8*np.eye(2)
+    alg['bsqkf'].tf_meas.model.model_var = 1e-3*np.eye(4)
+    print('BSQ EMV\ndyn: {} \nobs: {}'.format(alg['bsqkf'].tf_dyn.model.model_var.diagonal(),
+                                              alg['bsqkf'].tf_meas.model.model_var.diagonal()))
+
+    num_alg = len(alg)
+    d, steps, mc_sims = x.shape
+    mean, cov = np.zeros((d, steps, mc_sims, num_alg)), np.zeros((d, d, steps, mc_sims, num_alg))
+    for ia, a in enumerate(alg):
+        print('Running {:<5} ... '.format(a.upper()), end='', flush=True)
+        t0 = time.time()
+        for imc in range(mc_sims):
+            mean[..., imc, ia], cov[..., imc, ia] = alg[a].forward_pass(y[..., imc])
+            alg[a].reset()
+        print('{:>30}'.format('Done in {:.2f} [sec]'.format(time.time() - t0)))
+
+    # Performance score plots
+    print()
+    print('Computing performance scores ...')
+    error2 = mean.copy()
+    lcr_pos = np.zeros((steps, mc_sims, num_alg))
+    lcr_vel = lcr_pos.copy()
+    lcr_theta = lcr_pos.copy()
+    for a in range(num_alg):
+        for k in range(steps):
+            mse = mse_matrix(x[:, k, :], mean[:, k, :, a])
+            for imc in range(mc_sims):
+                error2[:, k, imc, a] = squared_error(x[:, k, imc], mean[:, k, imc, a])
+                cov_sel = cov[..., k, imc, a]
+                lcr_pos[k, imc, a] = log_cred_ratio(x[(0, 2), k, imc], mean[(0, 2), k, imc, a],
+                                                    cov_sel[np.ix_((0, 2), (0, 2))], mse[np.ix_((0, 2), (0, 2))])
+                lcr_vel[k, imc, a] = log_cred_ratio(x[(1, 3), k, imc], mean[(1, 3), k, imc, a],
+                                                    cov_sel[np.ix_((1, 3), (1, 3))], mse[np.ix_((1, 3), (1, 3))])
+                lcr_theta[k, imc, a] = log_cred_ratio(x[4, k, imc], mean[4, k, imc, a],
+                                                      cov[4, 4, k, imc, a], mse[4, 4])
+
+    # Averaged RMSE and Inclination Indicator in time
+    pos_rmse_vs_time = np.sqrt((error2[(0, 2), ...]).sum(axis=0)).mean(axis=1)
+    vel_rmse_vs_time = np.sqrt((error2[(1, 3), ...]).sum(axis=0)).mean(axis=1)
+    theta_rmse_vs_time = np.sqrt((error2[4, ...])).mean(axis=1)
+    pos_inc_vs_time = lcr_pos.mean(axis=1)
+    vel_inc_vs_time = lcr_vel.mean(axis=1)
+    theta_inc_vs_time = lcr_theta.mean(axis=1)
+
+    # print performance scores
+    print('Average position RMSE: {}'.format(pos_rmse_vs_time.mean(axis=0)))
+    print('Average position Inc.: {}'.format(pos_inc_vs_time.mean(axis=0)))
+    print('Average velocity RMSE: {}'.format(vel_rmse_vs_time.mean(axis=0)))
+    print('Average velocity Inc.: {}'.format(vel_inc_vs_time.mean(axis=0)))
+    print('Average parameter RMSE: {}'.format(theta_rmse_vs_time.mean(axis=0)))
+    print('Average parameter Inc.: {}'.format(theta_inc_vs_time.mean(axis=0)))
+
+    # PLOTS # TODO: figures for states rather than scores
+    # root mean squared error
+    plt.figure().suptitle('RMSE')
+    g = GridSpec(3, 1)
+    ax = plt.subplot(g[0, 0])
+    ax.set_title('Position')
+    for i, f_str in enumerate(alg):
+        ax.plot(pos_rmse_vs_time[:, i], label=f_str.upper())
+    ax.legend()
+    ax = plt.subplot(g[1, 0])
+    ax.set_title('Velocity')
+    for i, f_str in enumerate(alg):
+        ax.plot(vel_rmse_vs_time[:, i])
+    ax = plt.subplot(g[2, 0])
+    ax.set_title('Parameter')
+    for i, f_str in enumerate(alg):
+        ax.plot(theta_rmse_vs_time[:, i])
+    plt.show(block=False)
+
+    # inclination
+    plt.figure().suptitle('Inclination')
+    ax = plt.subplot(g[0, 0])
+    ax.set_title('Position')
+    for i, f_str in enumerate(alg):
+        ax.plot(pos_inc_vs_time[:, i], label=f_str.upper())
+    ax.legend()
+    ax = plt.subplot(g[1, 0])
+    ax.set_title('Velocity')
+    for i, f_str in enumerate(alg):
+        ax.plot(vel_inc_vs_time[:, i])
+    ax = plt.subplot(g[2, 0])
+    ax.set_title('Parameter')
+    for i, f_str in enumerate(alg):
+        ax.plot(theta_inc_vs_time[:, i])
+    plt.show()
+
+
 def multivariate_emv(tf, par, multi_ind):
     import scipy.linalg as la
     from ssmtoybox.utils import vandermonde
@@ -357,4 +488,6 @@ def multivariate_emv(tf, par, multi_ind):
 
 
 if __name__ == '__main__':
+    # reentry_simple_demo()
     reentry_demo()
+    # coordinated_turn_radar_demo()
