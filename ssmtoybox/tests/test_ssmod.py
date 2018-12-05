c399bcdb678b997c06010455517d8304605e3c5d
Jakub Pruher <jacobnzw@gmail.com>
2018-12-05 12:14:28 +0100

    CTRS model simulation test + visuals for verification.

--- ssmtoybox/tests/test_ssmod.py
+++ ssmtoybox/tests/test_ssmod.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from ssmtoybox.ssinf import ExtendedKalman, CubatureKalman, UnscentedKalman, GaussHermiteKalman, GaussianProcessKalman, TPQKalman
 from ssmtoybox.ssmod import UNGMTransition, UNGMMeasurement, UNGMNATransition, UNGMNAMeasurement, \
-    Pendulum2DTransition, Pendulum2DMeasurement
+    Pendulum2DTransition, Pendulum2DMeasurement, ConstantTurnRateSpeed, Radar2DMeasurement
 from ssmtoybox.utils import GaussRV
 
 
@@ -39,100 +39,28 @@ class TestUNGM(unittest.TestCase):
         x = ungmna_dyn.simulate_discrete(time_steps, mc_sims=20)
         y = ungmna_meas.simulate_measurements(x)
 
-    def test_ungm_inference(self):  # FIXME: these methods test the filters, not the SSMs; should be in test_ssinf.py
-        """
-        Test bunch of filters on Univariate Non-linear Growth Model (with additive noise)
-        """
-        dim = 1
-        init_dist = GaussRV(dim)
-        noise_dist = GaussRV(dim, cov=np.atleast_2d(10.0))
-        dyn = UNGMTransition(init_dist, noise_dist)
-        obs = UNGMMeasurement(GaussRV(dim), dyn.dim_state)
-        x = dyn.simulate_discrete(100, mc_sims=1)
-        z = obs.simulate_measurements(x)
-        hyp_dyn, hyp_meas = default_bq_hypers(dyn, obs)
-        inf_method = (
-            ExtendedKalman(dyn, obs),
-            UnscentedKalman(dyn, obs),
-            CubatureKalman(dyn, obs),
-            GaussHermiteKalman(dyn, obs),
-            GaussianProcessKalman(dyn, obs, hyp_dyn, hyp_meas),
-            TPQKalman(dyn, obs, hyp_dyn, hyp_meas),
-        )
-        for inf in inf_method:
-            inf.forward_pass(z[..., 0])
-            inf.backward_pass()
-
-        # import matplotlib.pyplot as plt
-        # plt.figure()
-        # for inf in inf_method:
-        #     plt.plot(inf.fi_mean[:, 1:].T, label=inf.__class__.__name__)
-        # plt.plot(x[..., 0].T, label='True', color='r', ls='--')
-        # plt.legend()
-        # plt.show()
-
-    def test_ungm_nonadd_inference(self):
-        """
-        Test bunch of filters on Univariate Non-linear Growth Model (with NON-additive noise)
-        """
-        dim = 1
-        init_dist = GaussRV(dim)
-        noise_dist = GaussRV(dim, cov=np.atleast_2d(10.0))
-        dyn = UNGMNATransition(init_dist, noise_dist)
-        obs = UNGMNAMeasurement(GaussRV(dim), dyn.dim_state)
-        x = dyn.simulate_discrete(100)
-        z = obs.simulate_measurements(x)
-        hyp_dyn, hyp_meas = default_bq_hypers(dyn, obs)
-        inf_method = (
-            ExtendedKalman(dyn, obs),
-            UnscentedKalman(dyn, obs),
-            CubatureKalman(dyn, obs),
-            GaussHermiteKalman(dyn, obs),
-            GaussianProcessKalman(dyn, obs, hyp_dyn, hyp_meas),
-            TPQKalman(dyn, obs, hyp_dyn, hyp_meas),
-        )
-        for inf in inf_method:
-            print(r"Testing {} ...".format(inf.__class__.__name__), end=' ')
-            try:
-                inf.forward_pass(z[..., 0])
-                inf.backward_pass()
-            except BaseException as e:
-                print("Failed {}".format(e))
-                continue
-            print("OK")
-
 
 class TestPendulum(unittest.TestCase):
-    def test_pendulum_inference(self):
-        """
-        Test bunch of filters on a pendulum example
-        """
-
-        # transition model: x0 = initial state RV, q = state noise RV, dt = discretization period
-        x0 = GaussRV(2, mean=np.array([1.5, 0]), cov=0.01 * np.eye(2))
-        dt = 0.01
-        q = GaussRV(2, cov=0.01 * np.array([[(dt ** 3) / 3, (dt ** 2) / 2], [(dt ** 2) / 2, dt]]))
-        dyn = Pendulum2DTransition(x0, q, dt=dt)
-
-        # measurement model: r = measurement noise RV
-        r = GaussRV(1, cov=np.array([[0.1]]))
-        obs = Pendulum2DMeasurement(r, dyn.dim_state)
-
-        x = dyn.simulate_discrete(100)
-        z = obs.simulate_measurements(x)
-        hyp_dyn, hyp_meas = default_bq_hypers(dyn, obs)
-        inf_method = (
-            ExtendedKalman(dyn, obs),
-            UnscentedKalman(dyn, obs),
-            CubatureKalman(dyn, obs),
-            GaussHermiteKalman(dyn, obs),
-            GaussianProcessKalman(dyn, obs, hyp_dyn, hyp_meas),
-            TPQKalman(dyn, obs, hyp_dyn, hyp_meas),
-        )
-        for inf in inf_method:
-            inf.forward_pass(z[..., 0])
-            inf.backward_pass()
+    pass
 
 
 class TestReentry(unittest.TestCase):
     pass
+
+
+class TestCTRS(unittest.TestCase):
+
+    def test_simulate(self):
+        # setup CTRS with radar measurements
+        x0 = GaussRV(5, cov=0.1 * np.eye(5))
+        q = GaussRV(2, cov=np.diag([0.1, 0.1 * np.pi]))
+        r = GaussRV(2, cov=np.diag([0.3, 0.03]))
+        dyn = ConstantTurnRateSpeed(x0, q)
+        obs = Radar2DMeasurement(r, 5)
+        x = dyn.simulate_discrete(100, 10)
+        y = obs.simulate_measurements(x)
+
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.plot(x[0, ...], x[1, ...], alpha=0.25, color='b')
+        plt.show()
