229dafe69540f8ff4447cf0e295cc6feedc60c3f
Jakub Prüher <jacobnzw@gmail.com>
2019-02-18 16:44:45 +0100

    Updated setuptools.

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup, find_packages
+import setuptools
 
+with open('README.md') as file:
+    ld = file.read()
 
-setup(
+setuptools.setup(
     name='ssmtoybox',
     version='0.1.1a0',
     url='https://github.com/jacobnzw/SSMToybox/tree/v0.1.1-alpha',
@@ -9,9 +11,18 @@ setup(
     author='Jakub Prüher',
     author_email='jacobnzw@gmail.com',
     description='Local filters based on Bayesian quadrature',
-    long_description=open('README.md').read(),
-    packages=find_packages(),
+    long_description=ld,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
     zip_safe=False,
     setup_requires=['nose>=1.0'],
-    test_suite='nose.collector'
+    test_suite='nose.collector',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+    ]
 )
