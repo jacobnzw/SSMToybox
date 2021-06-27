101cbf0ec89c8ec2ca2fd2085b250c40380af672
Jakub Prüher <jacobnzw@gmail.com>
2021-06-27 18:07:15 +0200

    Kernel expectations coded, preliminary tests passing.

--- research/gpqd/mlsp2016_demo.py
+++ research/gpqd/mlsp2016_demo.py
@@ -12,13 +12,13 @@ from ssmtoybox.mtran import LinearizationTransform, TaylorGPQDTransform, MonteCa
     GaussHermiteTransform, SphericalRadialTransform
 from ssmtoybox.bq.bqmtran import BQTransform, GaussianProcessTransform
 from ssmtoybox.bq.bqmod import GaussianProcessModel
-from ssmtoybox.bq.bqkern import RBFGauss
+from ssmtoybox.bq.bqkern import RBFGauss, Kernel
 from ssmtoybox.ssmod import UNGMTransition, UNGMMeasurement
 
 from scipy.stats import norm
 from scipy.linalg import cho_factor, cho_solve
 
-from bayesquad import GPQuadDerRBF  # TODO: port the GPQ+D for any point-set from GPQuadDerRBF
+# from research.gpqd.bayesquad import GPQuadDerRBF  # TODO: port the GPQ+D for any point-set from GPQuadDerRBF
 
 # Plan to port GPQuadDerRBF GP Quadrature moment transform with derivative observations
 # =====================================================================================
@@ -158,41 +158,206 @@ class RBFGaussDer(RBFGauss):
     def __init__(self, dim, par, jitter=1e-8):
         super(RBFGaussDer, self).__init__(dim, par, jitter)
 
-    def eval(self, par, x1, x2=None, diag=False, scaling=True):
-        # TODO: add evaluation of the Kfd and Kdd
+    def eval(self, par, x1, x2=None, diag=False, scaling=True, which_der=None):
+
         if x2 is None:
             x2 = x1.copy()
 
         alpha, sqrt_inv_lam = RBFGauss._unpack_parameters(par)
         alpha = 1.0 if not scaling else alpha
 
-        x1 = sqrt_inv_lam.dot(x1)
+        x1 = sqrt_inv_lam.dot(x1)  # sqrt(Lam^-1) * x
         x2 = sqrt_inv_lam.dot(x2)
         if diag:  # only diagonal of kernel matrix
             assert x1.shape == x2.shape
             dx = x1 - x2
-            return np.exp(2 * np.log(alpha) - 0.5 * np.sum(dx * dx, axis=0))
+            Kff = np.exp(2 * np.log(alpha) - 0.5 * np.sum(dx * dx, axis=0))
         else:
-            return np.exp(2 * np.log(alpha) - 0.5 * maha(x1.T, x2.T))
+            Kff = np.exp(2 * np.log(alpha) - 0.5 * maha(x1.T, x2.T))
+
+        x1, x2 = np.atleast_2d(x1), np.atleast_2d(x2)
+        D, N = x1.shape
+        Ds, Ns = x2.shape
+        assert Ds == D
+        which_der = np.arange(N) if which_der is None else which_der
+        Nd = len(which_der)  # points w/ derivative observations
+        # iLam = np.diag(el ** -1 * np.ones(D))  # sqrt(Lam^-1)
+        # iiLam = np.diag(el ** -2 * np.ones(D))  # Lam^-1
+
+        # x1 = iLam.dot(x1)  # sqrt(Lambda^-1) * X
+        # x2 = iLam.dot(x2)
+        # Kff = np.exp(2 * np.log(alpha) - 0.5 * maha(x2.T, x1.T))  # cov(f(xi), f(xj))
+        x1 = sqrt_inv_lam.dot(x1)  # Lambda^-1 * X
+        x2 = sqrt_inv_lam.dot(x2)
+        inv_lam = sqrt_inv_lam ** 2
+        XmX = x2[..., na] - x1[:, na, :]  # pair-wise differences
+
+        # TODO: benchmark vs. np.kron(), replace with np.kron() if possible
+        Kfd = np.zeros((Ns, D * Nd))  # cov(f(xi), df(xj))
+        for i in range(Ns):
+            for j in range(Nd):
+                jstart, jend = j * D, j * D + D
+                j_d = which_der[j]
+                Kfd[i, jstart:jend] = Kff[i, j_d] * XmX[:, i, j_d]
+
+        Kdd = np.zeros((D * Nd, D * Nd))  # cov(df(xi), df(xj))
+        for i in range(Nd):
+            for j in range(Nd):
+                istart, iend = i * D, i * D + D
+                jstart, jend = j * D, j * D + D
+                i_d, j_d = which_der[i], which_der[j]  # indices of points with derivatives
+                Kdd[istart:iend, jstart:jend] = Kff[i_d, j_d] * (inv_lam - np.outer(XmX[:, i_d, j_d], XmX[:, i_d, j_d]))
+        if Ns == N:
+            return np.vstack((np.hstack((Kff, Kfd)), np.hstack((Kfd.T, Kdd))))
+        else:
+            return np.hstack((Kff, Kfd))
+
+    def eval_inv_dot(self, par, x, b=None, scaling=True, which_der=None):
+        """
+        Compute the product of kernel matrix inverse and a vector `b`.
+
+        Parameters
+        ----------
+        par : ndarray
+            Kernel parameters.
+
+        x : ndarray
+            Data set.
+
+        b : None or ndarray, optional
+            If `None`, inverse kernel matrix is computed (i.e. `b=np.eye(N)`).
+
+        scaling : bool, optional
+            Use scaling parameter of the kernel matrix.
+
+        which_der : ndarray
+            Indicates for which points are the derivatives available.
+
+        Returns
+        -------
+        : (N, N) ndarray
+            Product of kernel matrix inverse and vector `b`.
+        """
+        # if b=None returns inverse of K
+        dim, num_pts = x.shape
+        which_der = np.arange(num_pts) if which_der is None else which_der
+        num_der = len(which_der)  # number of points with derivatives
+        K = self.eval(par, x, scaling=scaling, which_der=which_der)
+        return self._cho_inv(K + self.jitter * np.eye(num_pts + num_der*dim), b)
+
+    def eval_chol(self, par, x, scaling=True, which_der=None):
+        """
+        Compute of Cholesky factor of the kernel matrix.
+
+        Parameters
+        ----------
+        par : (dim+1, ) ndarray
+            Kernel parameters.
+
+        x : (dim, N) ndarray
+            Data set.
+
+        scaling : bool, optional
+            Use scaling parameter of the kernel.
+
+        which_der : ndarray
+            Indicates for which points are the derivatives available.
+
+        Returns
+        -------
+        : (N, N) ndarray
+            Cholesky factor of the kernel matrix.
+        """
+        dim, num_pts = x.shape
+        which_der = np.arange(num_pts) if which_der is None else which_der
+        num_der = len(which_der)  # number of points with derivatives
+        K = self.eval(par, x, scaling=scaling, which_der=which_der)
+        return la.cholesky(K + self.jitter * np.eye(num_pts + num_der*dim))
 
     def exp_x_dkx(self, par, x, scaling=False, which_der=None):
-        pass
+        """Expectation E_x[k_fd(x, x_n)]"""
 
-    def exp_x_kxdkx(self, par_0, par_1, x, scaling=False, which_der=None):
-        alpha, sqrt_inv_lam = RBFGauss._unpack_parameters(par_0)
-        alpha_1, sqrt_inv_lam_1 = RBFGauss._unpack_parameters(par_1)
-        alpha, alpha_1 = (1.0, 1.0) if not scaling else (alpha, alpha_1)
+        dim, num_pts = x.shape
+        alpha, sqrt_inv_lam = RBFGauss._unpack_parameters(par)
+        # alpha = 1.0 if not scaling else alpha
         inv_lam = sqrt_inv_lam ** 2
-        inv_lam_1 = sqrt_inv_lam_1 ** 2
-        # TODO: evaluation of the E[k_ff(x_n, x) k_fd(x, x_m)]
+        lam = np.diag(inv_lam.diagonal() ** -1)
+        which_der = np.arange(num_pts) if which_der is None else which_der
 
-    def exp_x_dkxdkx(self, par_0, par_1, x, scaling=False, which_der=None):
-        alpha, sqrt_inv_lam = RBFGauss._unpack_parameters(par_0)
-        alpha_1, sqrt_inv_lam_1 = RBFGauss._unpack_parameters(par_1)
-        alpha, alpha_1 = (1.0, 1.0) if not scaling else (alpha, alpha_1)
+        q = self.exp_x_kx(par, x, scaling)  # kernel mean E_x[k_ff(x, x_n)]
+
+        eye_d = np.eye(dim)
+        Sig_q = cho_solve(cho_factor(inv_lam + eye_d), eye_d)  # B^-1*I
+        eta = Sig_q.dot(x)  # (D,N) Sig_q*x
+        mu_q = inv_lam.dot(eta)  # (D,N)
+        r = q[na, which_der] * inv_lam.dot(mu_q[:, which_der] - x[:, which_der])  # -t.dot(iLam) * q  # (D, N)
+
+        return np.hstack((q.T, r.T.ravel()))  # q_tilde (1, N + n_der*D)
+
+    def exp_x_kxdkx(self, par, x, scaling=False, which_der=None):
+        """Expectation E_x[k_ff(x_n, x) k_fd(x, x_m)]"""
+        dim, num_pts = x.shape
+        which_der = np.arange(num_pts) if which_der is None else which_der
+        num_der = len(which_der)
+        _, sqrt_inv_lam = RBFGauss._unpack_parameters(par)
+
         inv_lam = sqrt_inv_lam ** 2
-        inv_lam_1 = sqrt_inv_lam_1 ** 2
-        # TODO: evaluation of the E[k_df(x_n, x) k_fd(x, x_m)]
+        eye_d = np.eye(dim)
+
+        q = self.exp_x_kx(par, x, scaling)
+        Sig_q = cho_solve(cho_factor(inv_lam + eye_d), eye_d)  # B^-1*I
+        eta = Sig_q.dot(x)  # (D,N) Sig_q*x
+        mu_q = inv_lam.dot(eta)  # (D,N)
+        r = q[na, which_der] * inv_lam.dot(mu_q[:, which_der] - x[:, which_der])  # -t.dot(iLam) * q  # (D, N)
+
+        #  quantities for cross-covariance "weights"
+        iLamSig = inv_lam.dot(Sig_q)  # (D,D)
+        r_tilde = np.empty((dim, num_der * dim))
+        for i in range(num_der):
+            i_d = which_der[i]
+            r_tilde[:, i * dim:i * dim + dim] = q[i_d] * iLamSig + np.outer(mu_q[:, i_d], r[:, i].T)
+
+        return np.hstack((q[na, :] * mu_q, r_tilde))  # R_tilde (D, N+N*D)
+
+    def exp_x_dkxdkx(self, par, x, scaling=False, which_der=None):
+        """Expectation E_x[k_df(x_n, x) k_fd(x, x_m)]"""
+        dim, num_pts = x.shape
+        which_der = np.arange(num_pts) if which_der is None else which_der
+        num_der = len(which_der)
+
+        _, sqrt_inv_lam = RBFGauss._unpack_parameters(par)
+        inv_lam = sqrt_inv_lam ** 2
+        lam = np.diag(inv_lam.diagonal() ** -1)
+        eye_d = np.eye(dim)
+
+        # quantities for covariance weights
+        Sig_q = cho_solve(cho_factor(inv_lam + eye_d), eye_d)  # B^-1*I
+        eta = Sig_q.dot(x)  # (D,N) Sig_q*x
+        inn = inv_lam.dot(x)  # inp / el[:, na]**2
+        Q = self.exp_x_kxkx(par, par, x, scaling)  # (N,N)
+
+        cho_LamSig = cho_factor(lam + Sig_q)
+        Sig_Q = cho_solve(cho_LamSig, Sig_q).dot(inv_lam)  # (D,D) Lambda^-1 (Lambda*(Lambda+Sig_q)^-1*Sig_q) Lambda^-1
+        eta_tilde = inv_lam.dot(cho_solve(cho_LamSig, eta))  # Lambda^-1(Lambda+Sig_q)^-1*eta
+        mu_Q = eta_tilde[..., na] + eta_tilde[:, na, :]  # (D,N_der,N) pairwise sum of pre-multiplied eta's
+
+        E_dfff = np.empty((num_der * dim, num_pts))
+        for i in range(num_der):
+            for j in range(num_pts):
+                istart, iend = i * dim, i * dim + dim
+                i_d = which_der[i]
+                E_dfff[istart:iend, j] = Q[i_d, j] * (mu_Q[:, i_d, j] - inn[:, i_d])
+
+        E_dffd = np.empty((num_der * dim, num_der * dim))
+        for i in range(num_der):
+            for j in range(num_der):
+                istart, iend = i * dim, i * dim + dim
+                jstart, jend = j * dim, j * dim + dim
+                i_d, j_d = which_der[i], which_der[j]
+                T = np.outer((inn[:, i_d] - mu_Q[:, i_d, j_d]), (inn[:, j_d] - mu_Q[:, i_d, j_d]).T) + Sig_Q
+                E_dffd[istart:iend, jstart:jend] = Q[i_d, j_d] * T
+
+        return np.vstack((np.hstack((Q, E_dfff.T)), np.hstack((E_dfff, E_dffd))))  # Q_tilde (N + N_der*D, N + N_der*D)
 
 
 def sos(x, pars, dx=False):
