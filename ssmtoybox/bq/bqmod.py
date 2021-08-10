Fix #29: the expected model variance of the StudentProcess, dim_out argument in TPQSF and TPQKF.

ssmtoybox/bq/bqmod.py
@@ -1144,15 +1144,17 @@ class StudentTProcessModel(GaussianProcessModel):
             Expected model variance.
         """
 
-        fcn_obs = np.squeeze(args[0])
+        fcn_obs = args[0]
         if self.estimate_par:
             # re-compute EMV if kernel parameters are being estimated
             iK = self.kernel.exp_x_kxkx(par, par, self.points)
-            scale = (self.nu - 2 + fcn_obs.dot(iK).dot(fcn_obs.T)) / (self.nu - 2 + self.num_pts)
+            quad_form = np.einsum('ij,jk,ik->i', fcn_obs, iK, fcn_obs)
+            scale = (self.nu - 2 + quad_form) / (self.nu - 2 + self.num_pts)
             gp_emv = super(StudentTProcessModel, self).exp_model_variance(par)
         else:
             # otherwise use pre-computed values (based on unit sigma-points and kernel parameters given at init)
-            scale = (self.nu - 2 + fcn_obs.dot(self.iK).dot(fcn_obs.T)) / (self.nu - 2 + self.num_pts)
+            quad_form = np.einsum('ij,jk,ik->i', fcn_obs, self.iK, fcn_obs)
+            scale = (self.nu - 2 + quad_form) / (self.nu - 2 + self.num_pts)
             gp_emv = self.model_var
         return scale * gp_emv
 

ssmtoybox/bq/bqmtran.py
@@ -413,8 +413,8 @@ class StudentTProcessTransform(BQTransform):
         : ndarray
             Transformed covariance.
         """
-        emv = self.model.exp_model_variance(self.model.kernel.par, fcn_evals) * self.I_out
-        return fcn_evals.dot(weights).dot(fcn_evals.T) - np.outer(mean_out, mean_out.T) + emv
+        emv = self.model.exp_model_variance(self.model.kernel.par, fcn_evals)
+        return fcn_evals.dot(weights).dot(fcn_evals.T) - np.outer(mean_out, mean_out.T) + np.diag(emv)
 
 
 """

ssmtoybox/ssinf.py
@@ -564,11 +564,11 @@ class StudentProcessKalman(GaussianInference):
         # specification of the moment transform for the transition model
         kernel_dyn = {'name': kernel, 'params': kern_par_dyn}
         points_dyn = {'name': points, 'params': point_hyp}
-        t_dyn = StudentTProcessTransform(dyn.dim_in, 1, kernel_dyn, points_dyn, nu=nu)
+        t_dyn = StudentTProcessTransform(dyn.dim_in, dyn.dim_state, kernel_dyn, points_dyn, nu=nu)
         # specification of the moment transform for the measurement model
         kernel_obs = {'name': kernel, 'params': kern_par_obs}
         points_obs = {'name': points, 'params': point_hyp}
-        t_obs = StudentTProcessTransform(obs.dim_in, 1, kernel_obs, points_obs, nu=nu)
+        t_obs = StudentTProcessTransform(obs.dim_in, obs.dim_out, kernel_obs, points_obs, nu=nu)
 
         super(StudentProcessKalman, self).__init__(dyn, obs, t_dyn, t_obs)
 
@@ -802,10 +802,10 @@ class FullySymmetricStudent(StudentianInference):
     """
 
     def __init__(self, dyn, obs, degree=3, kappa=None, dof=4.0):
-        dyn_dof = np.min((dyn.init_rv.dof, dyn.noise_rv.dof))  # FIXME: if fixed_dof, need to pass in dof, no heuristics
+        dyn_dof = np.min((dyn.init_rv.dof, dyn.noise_rv.dof))
         obs_dof = np.min((dyn_dof, obs.noise_rv.dof))
-        t_dyn = FullySymmetricStudentTransform(dyn.dim_in, degree, kappa, dof)
-        t_obs = FullySymmetricStudentTransform(obs.dim_in, degree, kappa, dof)
+        t_dyn = FullySymmetricStudentTransform(dyn.dim_in, degree, kappa, dyn_dof)
+        t_obs = FullySymmetricStudentTransform(obs.dim_in, degree, kappa, obs_dof)
         super(FullySymmetricStudent, self).__init__(dyn, obs, t_dyn, t_obs, dof)
 
 
@@ -865,11 +865,11 @@ class StudentProcessStudent(StudentianInference):
         # specification of the moment transform for the transition model
         kernel_dyn = {'name': 'rbf-student', 'params': kern_par_dyn}
         points_dyn = {'name': 'fs', 'params': point_par_dyn}
-        t_dyn = StudentTProcessTransform(dyn.dim_in, 1, kernel_dyn, points_dyn, nu=dof_tp)
+        t_dyn = StudentTProcessTransform(dyn.dim_in, dyn.dim_state, kernel_dyn, points_dyn, nu=dof_tp)
         # specification of the moment transform for the measurement model
         kernel_obs = {'name': 'rbf-student', 'params': kern_par_obs}
         points_obs = {'name': 'fs', 'params': point_par_obs}
-        t_obs = StudentTProcessTransform(obs.dim_in, 1, kernel_obs, points_obs, nu=dof_tp)
+        t_obs = StudentTProcessTransform(obs.dim_in, obs.dim_out, kernel_obs, points_obs, nu=dof_tp)
 
         super(StudentProcessStudent, self).__init__(dyn, obs, t_dyn, t_obs, dof)
 